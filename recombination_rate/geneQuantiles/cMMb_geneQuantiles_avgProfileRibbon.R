31fa65964e387958cd59f9c74bc49d5f6e839ea4
ajtock <andytock@gmail.com>
2019-07-01 13:40:08 +0100

    update

--- recombination_rate/geneQuantiles/cMMb_geneQuantiles_avgProfileRibbon.R
+++ recombination_rate/geneQuantiles/cMMb_geneQuantiles_avgProfileRibbon.R
@@ -190,7 +190,7 @@ genesGR <- genesGR[genesGR$cMMb != "NaN"]
 
 # Separate genes into genomes
 genomeNames <- c("A", "B", "D", "")
-genomeNamesPlot <- c("A", "B", "D", "all")
+genomeNamesPlot <- c("A", "B", "D", "All")
 genesGR_genomeList <- lapply(seq_along(genomeNames), function(z) {
   genesGR[grep(genomeNames[z],
                seqnames(genesGR))]
@@ -207,7 +207,7 @@ sapply(seq_along(genesGR_genomeList), function(z) {
   quantilesCum <- cumsum(c(1,
                            rep(quantilesN,
                                times = quantiles)))
-  if(quantilesCum[length(quantilesCum)] < dim(genesDF_ordered)[1]) {
+  if(quantilesCum[length(quantilesCum)] < dim(genesDF_ordered)[2]) {
     quantilesCum <- c(quantilesCum, dim(genesDF_ordered)[1])
   }
   quantilesStats <- data.frame()
@@ -229,11 +229,11 @@ sapply(seq_along(genesGR_genomeList), function(z) {
       print(dim(quantilejFeatures))
     }
     if(j <= quantiles) {
-      stats <-  data.frame(quantile = as.integer(j),
-                           n = as.integer(dim(quantilejFeatures)[1]),
-                           mean_width = as.integer(round(mean(quantilejFeatures$width))),
-                           total_width = as.integer(sum(quantilejFeatures$width)),
-                           mean_cMMb = as.numeric(mean(quantilejFeatures$cMMb)))
+      stats <- data.frame(quantile = as.integer(j),
+                          n = as.integer(dim(quantilejFeatures)[1]),
+                          mean_width = as.integer(round(mean(quantilejFeatures$width, na.rm = T))),
+                          total_width = as.integer(sum(quantilejFeatures$width, na.rm = T)),
+                          mean_cMMb = as.numeric(mean(quantilejFeatures$cMMb, na.rm = T)))
     quantilesStats <- rbind(quantilesStats, stats)
     write.table(quantilejFeatures,
                 file = paste0(regionDir,
@@ -258,7 +258,7 @@ genesDF_genomeList_quantiles <- lapply(seq_along(genesGR_genomeList),
   function(z) {
     lapply(seq_along(1:quantiles), function(j) {
       tmp <- read.table(paste0(regionDir,
-                               "genes_in_", genomeNames[z], "genome_", region,
+                               featureName, "_in_", genomeNames[z], "genome_", region,
                                "_quantile", j, "_of_", quantiles, "quantiles_", 
                                "ordered_by_", winName, "Scaled_cMMb_",
                                "minInterMarkerDist", as.character(minMarkerDist), "bp.txt"),
@@ -430,7 +430,170 @@ for(z in seq_along(summaryDFfeature_genome)) {
 }
 
 
+## Random gene quantiles
+# Define function to randomly select n rows from
+# a GRanges object (features)
+selectRandomFeatures <- function(features, n) {
+  return(features[sample(x = dim(features)[1],
+                         size = n,
+                         replace = FALSE),])
+}
 
+# Define seed so that random selections are reproducible
+set.seed(93750174)
+
+# Divide genes into random sets of equal number,
+# with the same number of genes per chromosome as
+# above-defined cM/Mb-ordered gene quantiles 
+sapply(seq_along(genesDF_genomeList_quantiles), function(z) {
+  genesDF <- data.frame(genesGR_genomeList[[z]])
+  randomPCsStats <- data.frame()
+  for(j in 1:quantiles) {
+    print(j)
+    randomPCjFeatures <- data.frame()
+    for(i in 1:length(chrs)) {
+      randomPCjFeaturesChr <- selectRandomFeatures(features = genesDF[genesDF$seqnames
+                                                                      == chrs[i],],
+                                                   n = dim(genesDF_genomeList_quantiles[[z]][[j]][genesDF_genomeList_quantiles[[z]][[j]]$seqnames
+                                                                                                  == chrs[i],])[1])
+      randomPCjFeatures <- rbind(randomPCjFeatures, randomPCjFeaturesChr)
+    }
+    print(dim(randomPCjFeatures))
+    stats <- data.frame(quantile = as.integer(j),
+                        n = as.integer(dim(randomPCjFeatures)[1]),
+                        mean_width = as.integer(round(mean(randomPCjFeatures$width, na.rm = T))),
+                        total_width = as.integer(sum(randomPCjFeatures$width, na.rm = T)),
+                        mean_cMMb = as.numeric(mean(randomPCjFeatures$cMMb, na.rm = T)))
+    randomPCsStats <- rbind(randomPCsStats, stats)
+    write.table(randomPCjFeatures,
+                file = paste0(regionDir,
+                              featureName, "_in_", genomeNames[z], "genome_", region,
+                              "_quantile", j, "_of_", quantiles, "quantiles_randomPerChrom_",
+                              winName, "Scaled_cMMb_",
+                              "minInterMarkerDist", as.character(minMarkerDist), "bp.txt"),
+                quote = FALSE, sep = "\t", row.names = FALSE)
+  }
+  write.table(randomPCsStats,
+              file = paste0(regionDir,
+                            "summary_", featureName, "_in_", genomeNames[z], "genome_", region, "_",
+                            quantiles, "quantiles_randomPerChrom_",
+                            winName, "Scaled_cMMb_",
+                            "minInterMarkerDist", as.character(minMarkerDist), "bp.txt"),
+              quote = FALSE, sep = "\t", row.names = FALSE)
+})
+
+# Load randomPCs and order by geneID
+genesDF_genomeList_randomPCs <- lapply(seq_along(genesGR_genomeList),
+  function(z) {
+    lapply(seq_along(1:quantiles), function(j) {
+      tmp <- read.table(paste0(regionDir,
+                               featureName, "_in_", genomeNames[z], "genome_", region,
+                               "_quantile", j, "_of_", quantiles, "quantiles_randomPerChrom_",
+                               winName, "Scaled_cMMb_",
+                               "minInterMarkerDist", as.character(minMarkerDist), "bp.txt"),
+                        header = T)
+      #tmp[order(tmp$cMMb,
+      #          decreasing = T,
+      #          na.last = NA),]
+      tmp[order(tmp$geneID,
+                decreasing = F,
+                na.last = NA),]
+    })
+})
+
+# Subdivide coverage matrix into above-defined randomPCs
+log2covMat_genomeList_randomPCs <- lapply(seq_along(genesDF_genomeList_randomPCs),
+  function(z) {
+    lapply(seq_along(1:quantiles), function(j) {
+      tmp1 <- log2covMat[log2covMat$geneID %in% genesDF_genomeList_randomPCs[[z]][[j]]$geneID,]
+      tmp1[order(tmp1$geneID,
+                 decreasing = F,
+                 na.last = NA),]
+    })
+})
+
+
+## randomF
+# Transpose matrix and convert to dataframe
+# in which first column is window name
+wideDFrandomF_list <- mclapply(seq_along(log2covMat_genomeList_randomPCs), function(z) {
+  lapply(seq_along(1:quantiles), function(j) {
+    data.frame(window = colnames(as.matrix(log2covMat_genomeList_randomPCs[[z]][[j]][,-1])),
+               t(as.matrix(log2covMat_genomeList_randomPCs[[z]][[j]][,-1])))
+    })
+}, mc.cores = length(log2covMat_genomeList_randomPCs))
+
+# Convert into tidy data.frame (long format)
+tidyDFrandomF_list  <- mclapply(seq_along(wideDFrandomF_list), function(z) {
+  lapply(seq_along(1:quantiles), function(j) {
+    gather(data  = wideDFrandomF_list[[z]][[j]],
+           key   = randomF,
+           value = coverage,
+           -window)
+  })
+}, mc.cores = length(wideDFrandomF_list))
+
+# Order levels of factor "window" so that sequential levels
+# correspond to sequential windows
+for(z in seq_along(tidyDFrandomF_list)) {
+  for(j in seq_along(1:quantiles)) {
+    tidyDFrandomF_list[[z]][[j]]$window <- factor(tidyDFrandomF_list[[z]][[j]]$window,
+                                                  levels = as.character(wideDFrandomF_list[[z]][[j]]$window))
+  }
+}
+
+# Create summary data.frame in which each row corresponds to a window (Column 1),
+# Column2 is the number of coverage values (randomFs) per window,
+# Column3 is the mean of coverage values per window,
+# Column4 is the standard deviation of coverage values per window,
+# Column5 is the standard error of the mean of coverage values per window,
+# Column6 is the lower bound of the 95% confidence interval, and
+# Column7 is the upper bound of the 95% confidence interval
+summaryDFrandomF_list <- mclapply(seq_along(tidyDFrandomF_list), function(z) {
+  lapply(seq_along(1:quantiles), function(j) {
+    data.frame(window = as.character(wideDFrandomF_list[[z]][[j]]$window),
+               n      = tapply(X     = tidyDFrandomF_list[[z]][[j]]$coverage,
+                               INDEX = tidyDFrandomF_list[[z]][[j]]$window,
+                               FUN   = length),
+               mean   = tapply(X     = tidyDFrandomF_list[[z]][[j]]$coverage,
+                               INDEX = tidyDFrandomF_list[[z]][[j]]$window,
+                               FUN   = mean),
+               sd     = tapply(X     = tidyDFrandomF_list[[z]][[j]]$coverage,
+                               INDEX = tidyDFrandomF_list[[z]][[j]]$window,
+                               FUN   = sd))
+  })
+}, mc.cores = length(tidyDFrandomF_list))
+
+for(z in seq_along(summaryDFrandomF_list)) {
+  for(j in seq_along(1:quantiles)) {
+    summaryDFrandomF_list[[z]][[j]]$window <- factor(summaryDFrandomF_list[[z]][[j]]$window,
+                                                     levels = as.character(wideDFrandomF_list[[z]][[j]]$window))
+    summaryDFrandomF_list[[z]][[j]]$winNo <- factor(1:dim(summaryDFrandomF_list[[z]][[j]])[1])
+    summaryDFrandomF_list[[z]][[j]]$sem <- summaryDFrandomF_list[[z]][[j]]$sd/sqrt(summaryDFrandomF_list[[z]][[j]]$n-1)
+    summaryDFrandomF_list[[z]][[j]]$CI_lower <- summaryDFrandomF_list[[z]][[j]]$mean -
+      qt(0.975, df = summaryDFrandomF_list[[z]][[j]]$n-1)*summaryDFrandomF_list[[z]][[j]]$sem
+    summaryDFrandomF_list[[z]][[j]]$CI_upper <- summaryDFrandomF_list[[z]][[j]]$mean +
+      qt(0.975, df = summaryDFrandomF_list[[z]][[j]]$n-1)*summaryDFrandomF_list[[z]][[j]]$sem
+  }
+}
+
+randomPCNames <- paste0(rep("Random ", quantiles), 1:quantiles)
+for(z in seq_along(summaryDFrandomF_list)) {
+  names(summaryDFrandomF_list[[z]]) <- randomPCNames
+}
+
+# For each genome, convert list of lists summaryDFrandomF_list[[z]]
+# into a single data.frame of quantiles for plotting
+summaryDFrandomF_genome <- lapply(seq_along(summaryDFrandomF_list), function(z) {
+  bind_rows(summaryDFrandomF_list[[z]], .id = "quantile")
+})
+for(z in seq_along(summaryDFrandomF_genome)) {
+  summaryDFrandomF_genome[[z]]$quantile <- factor(summaryDFrandomF_genome[[z]]$quantile,
+                                                  levels = names(summaryDFrandomF_list[[z]]))
+}
+
+
+# Specify x-axis tick labels
 if(featureName == "genes" | featureName == "NLRs") {
   featureStartLab <- "TSS"
   featureEndLab <- "TTS"
@@ -445,10 +608,12 @@ if(featureName == "genes" | featureName == "NLRs") {
 #ymax <- max(c(summaryDFfeature$mean+summaryDFfeature$sem,
 #              summaryDFranLoc$mean+summaryDFranLoc$sem))
 ymin <- min(sapply(seq_along(summaryDFfeature_genome), function(z) {
-  min(c(summaryDFfeature_genome[[z]]$CI_lower))
+  min(c(summaryDFfeature_genome[[z]]$CI_lower),
+      c(summaryDFrandomF_genome[[z]]$CI_lower))
 }))
 ymax <- max(sapply(seq_along(summaryDFfeature_genome), function(z) {
-  max(c(summaryDFfeature_genome[[z]]$CI_upper))
+  max(c(summaryDFfeature_genome[[z]]$CI_upper),
+      c(summaryDFrandomF_genome[[z]]$CI_upper))
 }))
 
 # Function for formatting y-axis labels
@@ -460,8 +625,13 @@ fmt_decimals <- function(decimals) {
 colours <- colorRampPalette(c("red", "blue"))(4)
 
 # Define legend labels
-legendLabs <- lapply(seq_along(quantileNames), function(x) {
+legendLabs_feature <- lapply(seq_along(quantileNames), function(x) {
   grobTree(textGrob(bquote(.(quantileNames[x])),
+                    x = legendPos[1], y = legendPos[2]-((x-1)*0.06), just = "left",
+                    gp = gpar(col = colours[x], fontsize = 18)))
+})
+legendLabs_randomF <- lapply(seq_along(randomPCNames), function(x) {
+  grobTree(textGrob(bquote(.(randomPCNames[x])),
                     x = legendPos[1], y = legendPos[2]-((x-1)*0.06), just = "left",
                     gp = gpar(col = colours[x], fontsize = 18)))
 })
@@ -488,8 +658,8 @@ for(z in seq_along(summaryDFfeature_genome)) {
                               fill = quantile),
                 alpha = 0.4) +
     scale_fill_manual(values = colours) +
-    scale_y_continuous(limits = c(ymin, ymax)) +
-                       #labels = function(x) sprintf("%6.3f", x)) +
+    scale_y_continuous(limits = c(ymin, ymax),
+                       labels = function(x) sprintf("%7.4f", x)) +
     scale_x_discrete(breaks = c(1,
                                 (upstream/binSize)+1,
                                 (dim(summaryDFfeature_list[[z]][[1]])[1])-(downstream/binSize),
@@ -504,10 +674,10 @@ for(z in seq_along(summaryDFfeature_genome)) {
                size = 1) +
     labs(x = "",
          y = bquote("Log"[2] * "(" * .(markChIP) * "/control)")) +
-    annotation_custom(legendLabs[[1]]) +
-    annotation_custom(legendLabs[[2]]) +
-    annotation_custom(legendLabs[[3]]) +
-    annotation_custom(legendLabs[[4]]) +
+    annotation_custom(legendLabs_feature[[1]]) +
+    annotation_custom(legendLabs_feature[[2]]) +
+    annotation_custom(legendLabs_feature[[3]]) +
+    annotation_custom(legendLabs_feature[[4]]) +
     theme_bw() +
     theme(
           axis.ticks = element_line(size = 1.0, colour = "black"),
@@ -532,8 +702,69 @@ for(z in seq_along(summaryDFfeature_genome)) {
                    .(prettyNum(summaryDFfeature_genome[[z]]$n[1]*quantiles,
                                big.mark = ",", trim = T)) *
                    ")"))
-  #ggObjGA <- grid.arrange(ggObj1, ggObj2, nrow = 1, ncol = 2)
-  ggObjGA <- ggObj1
+  ggObj2 <- NULL
+  ggObj2 <- ggplot(data = summaryDFrandomF_genome[[z]],
+                   mapping = aes(x = winNo,
+                                 y = mean,
+                                 group = quantile),
+                  ) +
+    geom_line(data = summaryDFrandomF_genome[[z]],
+              mapping = aes(colour = quantile),
+              size = 1) +
+    scale_colour_manual(values = colours) +
+    geom_ribbon(data = summaryDFrandomF_genome[[z]],
+                #mapping = aes(ymin = mean-sem,
+                #              ymax = mean+sem,
+                mapping = aes(ymin = CI_lower,
+                              ymax = CI_upper,
+                              fill = quantile),
+                alpha = 0.4) +
+    scale_fill_manual(values = colours) +
+    scale_y_continuous(limits = c(ymin, ymax),
+                       labels = function(x) sprintf("%7.4f", x)) +
+    scale_x_discrete(breaks = c(1,
+                                (upstream/binSize)+1,
+                                (dim(summaryDFrandomF_list[[z]][[1]])[1])-(downstream/binSize),
+                                dim(summaryDFrandomF_list[[z]][[1]])[1]),
+                     labels = c(paste0("-", flankNamePlot),
+                                featureStartLab,
+                                featureEndLab,
+                                paste0("+", flankNamePlot))) +
+    geom_vline(xintercept = c((upstream/binSize)+1,
+                              (dim(summaryDFrandomF_list[[z]][[1]])[1])-(downstream/binSize)),
+               linetype = "dashed",
+               size = 1) +
+    labs(x = "",
+         y = bquote("Log"[2] * "(" * .(markChIP) * "/control)")) +
+    annotation_custom(legendLabs_randomF[[1]]) +
+    annotation_custom(legendLabs_randomF[[2]]) +
+    annotation_custom(legendLabs_randomF[[3]]) +
+    annotation_custom(legendLabs_randomF[[4]]) +
+    theme_bw() +
+    theme(
+          axis.ticks = element_line(size = 1.0, colour = "black"),
+          axis.ticks.length = unit(0.25, "cm"),
+          axis.text.x = element_text(size = 22, colour = "black"),
+          axis.text.y = element_text(size = 18, colour = "black", family = "Luxi Mono"),
+          axis.title = element_text(size = 30, colour = "black"),
+          legend.position = "none",
+          #legend.text = element_text(size = 10),
+          #legend.background = element_rect(fill = "transparent"),
+          #legend.key = element_rect(colour = "transparent",
+          #                          fill = "transparent"),
+          #legend.title = element_blank(),
+          panel.grid = element_blank(),
+          panel.border = element_rect(size = 3.5, colour = "black"),
+          panel.background = element_blank(),
+          plot.margin = unit(c(0.3,0.9,0.0,0.3), "cm"),
+          plot.title = element_text(hjust = 0.5, size = 20)) +
+    ggtitle(bquote(.(genomeNamesPlot[z]) * "-genome" ~ .(region) ~ 
+                   .(featureName) ~ "(" * italic("n") ~ "=" ~
+                   # INEXACT - NEED TO CHANGE
+                   .(prettyNum(summaryDFrandomF_genome[[z]]$n[1]*quantiles,
+                               big.mark = ",", trim = T)) *
+                   ")"))
+  ggObjGA <- grid.arrange(ggObj1, ggObj2, nrow = 1, ncol = 2)
   ggsave(paste0(plotDir,
                 "log2_", libNameChIP, "_", libNameControl,
                 "_around_", genomeNames[z], "genome_", region, "_",
@@ -541,5 +772,5 @@ for(z in seq_along(summaryDFfeature_genome)) {
                 "ordered_by_", winName, "Scaled_cMMb_",
                 "minInterMarkerDist", as.character(minMarkerDist), "bp.pdf"),
          plot = ggObjGA,
-         height = 6.5, width = 7)
+         height = 8, width = 16)
 }
