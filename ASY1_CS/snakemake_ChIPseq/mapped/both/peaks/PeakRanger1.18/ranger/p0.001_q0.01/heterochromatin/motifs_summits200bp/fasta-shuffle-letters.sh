1b62a6ee3a4d7713c87cd62776357af2d0c168b9
ajtock <andytock@gmail.com>
2019-07-13 09:53:37 +0100

    peak motifs

---
+++ ASY1_CS/snakemake_ChIPseq/mapped/both/peaks/PeakRanger1.18/ranger/p0.001_q0.01/heterochromatin/motifs_summits200bp/fasta-shuffle-letters.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Usage:
+# ./fasta-shuffle-letters.sh ASY1_CS_Rep1_ChIP_peaks_minuslog10Qsorted_in_Agenome_heterochromatin_summits200bp 
+
+prefix=$1
+toolDir=/home/ajt200/meme/bin
+
+$toolDir/fasta-shuffle-letters ${prefix}.fa \
+                               ${prefix}_shuffled.fa
+$toolDir/fasta-shuffle-letters ${prefix}_randomLoci.fa \
+                               ${prefix}_randomLoci_shuffled.fa
