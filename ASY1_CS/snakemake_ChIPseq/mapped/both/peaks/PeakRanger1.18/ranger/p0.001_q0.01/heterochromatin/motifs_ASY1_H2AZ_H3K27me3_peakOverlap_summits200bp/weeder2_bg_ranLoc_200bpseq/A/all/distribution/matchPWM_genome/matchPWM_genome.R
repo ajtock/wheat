75cfa9c00630a37edf99c98d59e61e71696ae86d
ajtock <andytock@gmail.com>
2019-07-19 18:27:31 +0100

    matchPWM_genome

---
+++ ASY1_CS/snakemake_ChIPseq/mapped/both/peaks/PeakRanger1.18/ranger/p0.001_q0.01/heterochromatin/motifs_ASY1_H2AZ_H3K27me3_peakOverlap_summits200bp/weeder2_bg_ranLoc_200bpseq/A/all/distribution/matchPWM_genome/matchPWM_genome.R
@@ -0,0 +1,341 @@
+#!/applications/R/R-3.5.0/bin/Rscript
+
+# Identify genomic loci that match motifs enriched at ASY1 peaks
+# using motif position weight matrices (PWM)
+# Profile base composition (proportion) in regions flanking these loci
+
+# Usage:
+# ./matchPWM_genome.R 'A' 'heterochromatin' 100 100bp
+
+genomeName <- "A"
+region <- "heterochromatin"
+args <- commandArgs(trailingOnly = T)
+genomeName <- args[1]
+region <- args[2]
+flankSize <- as.numeric(args[3])
+flankName <- args[4]
+
+library(Biostrings)
+library(BSgenome.Taestivum.Cambridge.iwgscRefseqv1)
+genome <- Taestivum
+library(parallel)
+
+library(segmentSeq)
+library(regioneR)
+#library(zoo)
+library(TTR)
+
+plotDir <- "plots/"
+propDir <- paste0(plotDir, "proportion/")
+gffDir <- "gff/"
+bedDir <- "bed/"
+system(paste0("[ -d ", plotDir, " ] || mkdir ", plotDir))
+system(paste0("[ -d ", propDir, " ] || mkdir ", propDir))
+system(paste0("[ -d ", gffDir, " ] || mkdir ", gffDir))
+system(paste0("[ -d ", bedDir, " ] || mkdir ", bedDir))
+plotDir <- propDir
+ 
+# Genomic definitions
+chrs <- as.vector(read.table("/home/ajt200/analysis/wheat/sRNAseq_meiocyte_Martin_Moore/snakemake_sRNAseq/data/index/wheat_v1.0.fa.sizes")[,1])
+chrs <- chrs[-length(chrs)]
+chrLens <- as.vector(read.table("/home/ajt200/analysis/wheat/sRNAseq_meiocyte_Martin_Moore/snakemake_sRNAseq/data/index/wheat_v1.0.fa.sizes")[,2])
+chrLens <- chrLens[-length(chrLens)]
+centromereStart <- as.vector(read.table("/home/ajt200/analysis/wheat/wheat_IWGSC_WGA_v1.0_pseudomolecules/centromeres_outer_CENH3enriched_IWGSC_2018_Science_Table_S11_chr2AMiddleInterval_chr4ALeftmostInterval_chr4BRightmostInterval_chr5ARightmostInterval_chr7BRightTwoIntervals.txt")[,2])
+centromereEnd <- as.vector(read.table("/home/ajt200/analysis/wheat/wheat_IWGSC_WGA_v1.0_pseudomolecules/centromeres_outer_CENH3enriched_IWGSC_2018_Science_Table_S11_chr2AMiddleInterval_chr4ALeftmostInterval_chr4BRightmostInterval_chr5ARightmostInterval_chr7BRightTwoIntervals.txt")[,3])
+chrPartitions <- read.table("/home/ajt200/analysis/wheat/wheat_IWGSC_WGA_v1.0_pseudomolecules/chromosome_partitions_IWGSC_2018_Science_Table_S29.txt",
+                            header = TRUE)
+# Define region to be analysed
+if(region == "euchromatin") {
+  regionGR <- GRanges(seqnames = rep(chrPartitions$chrom, 2),
+                      ranges = IRanges(start = c(rep(1, dim(chrPartitions)[1]),
+                                                 chrPartitions$R2b_R3),
+                                       end = c(chrPartitions$R1_R2a,
+                                               chrLens)),
+                      strand = "*")
+  regionGR <- regionGR[grep(genomeName,
+                            seqnames(regionGR))@values]
+} else if(region == "heterochromatin") {
+  regionGR <- GRanges(seqnames = chrPartitions$chrom,
+                      ranges = IRanges(start = chrPartitions$R1_R2a+1,
+                                       end = chrPartitions$R2b_R3-1),
+                      strand = "*")
+  regionGR <- regionGR[grep(genomeName,
+                            seqnames(regionGR))@values]
+} else if(region == "centromeres") {
+  regionGR <- GRanges(seqnames = chrs,
+                      ranges = IRanges(start = centromereStart,
+                                       end = centromereEnd),
+                      strand = "*")
+  regionGR <- regionGR[grep(genomeName,
+                            seqnames(regionGR))@values]
+} else if(region == "pericentromeres") {
+  regionGR <- GRanges(seqnames = chrPartitions$chrom,
+                      ranges = IRanges(start = chrPartitions$R2a_C+1,
+                                       end = chrPartitions$C_R2b-1),
+                      strand = "*")
+  regionGR <- regionGR[grep(genomeName,
+                            seqnames(regionGR))@values]
+} else if(region == "genomewide") {
+  regionGR <- GRanges(seqnames = chrs,
+                      ranges = IRanges(start = rep(1, length(chrs)),
+                                       end = chrLens),
+                      strand = "*")
+  regionGR <- regionGR[grep(genomeName,
+                            seqnames(regionGR))@values]
+} else {
+  stop("region is not euchromatin, heterochromatin, centromeres, pericentromeres, or genomewide")
+}
+# Define region to be masked out of analysis
+if(region == "euchromatin") {
+  maskGR <- GRanges(seqnames = chrPartitions$chrom,
+                    ranges = IRanges(start = chrPartitions$R1_R2a+1,
+                                     end = chrPartitions$R2b_R3-1),
+                    strand = "*")
+  maskGR <- maskGR[grep(genomeName,
+                        seqnames(maskGR))@values]
+} else if(region == "heterochromatin") {
+  maskGR <- GRanges(seqnames = rep(chrPartitions$chrom, 2),
+                    ranges = IRanges(start = c(rep(1, dim(chrPartitions)[1]),
+                                               chrPartitions$R2b_R3),
+                                     end = c(chrPartitions$R1_R2a,
+                                             chrLens)),
+                    strand = "*")
+  maskGR <- maskGR[grep(genomeName,
+                        seqnames(maskGR))@values]
+} else if(region == "centromeres") {
+  maskGR <- GRanges(seqnames = rep(chrPartitions$chrom, 2),
+                    ranges = IRanges(start = c(rep(1, dim(chrPartitions)[1]),
+                                               centromereEnd+1),
+                                     end = c(centromereStart-1,
+                                             chrLens)),
+                    strand = "*")
+  maskGR <- maskGR[grep(genomeName,
+                        seqnames(maskGR))@values]
+} else if(region == "pericentromeres") {
+  maskGR <- GRanges(seqnames = rep(chrPartitions$chrom, 2),
+                    ranges = IRanges(start = c(rep(1, dim(chrPartitions)[1]),
+                                               chrPartitions$C_R2b),
+                                     end = c(chrPartitions$R2a_C,
+                                             chrLens)),
+                    strand = "*")
+  maskGR <- maskGR[grep(genomeName,
+                        seqnames(maskGR))@values]
+} else if(region == "genomewide") {
+  maskGR <- GRanges()
+  maskGR <- maskGR[grep(genomeName,
+                        seqnames(maskGR))@values]
+} else {
+  stop("region is not euchromatin, heterochromatin, centromeres, pericentromeres, or genomewide")
+}
+
+# Extract genomeName-subgenome chromosomes
+chrs <- chrs[grep(genomeName, chrs)]
+genomeNums <- grep(genomeName,
+                   names(genome))
+chrList <- mclapply(genomeNums, function(i) {
+  genome[[i]]
+}, mc.cores = length(genomeNums))
+names(chrList) <- names(genome)[genomeNums]
+
+# Get number of motif PWMs
+pwmNum <- as.numeric(system("ls -1 ../../MAT*.pwm | wc -l",
+                            intern = T))
+
+# Get PMM names
+pwmName <- sapply(seq_along(1:pwmNum), function(x) {
+  pwmName <- system(paste0("ls ../../MAT", x, "_*.pwm"),
+                    intern = T) 
+  pwmName <- substring(text = pwmName,
+                       first = 7)
+  sub(pattern = ".pwm",
+      replacement = "",
+      x = pwmName)
+})
+
+# Load PWMs as list
+pwmList <- lapply(1:pwmNum, function(x) {
+  as.matrix(read.table(system(paste0("ls ../../MAT", x, "_*.pwm"),
+                              intern = T),
+                       skip = 1, row.names = 1))
+})
+
+# Obtain motif matches in each genomeName-subgenome chromosome
+mclapply(seq_along(pwmList), function(x) {
+  hitsGR <- GRanges()
+  for(i in seq_along(chrList)) {
+    print(chrs[i])
+    hits <- matchPWM(pwm = pwmList[[x]],
+                     subject = chrList[[i]],
+                     min.score = "99%")
+    hitsGRchr <- GRanges(seqnames = chrs[i],
+                         ranges = hits@ranges,
+                         strand = "+")
+    hitsRC <- matchPWM(pwm = reverseComplement(pwmList[[x]]),
+                       subject = chrList[[i]],
+                       min.score = "99%")
+    hitsRCGRchr <- GRanges(seqnames = chrs[i],
+                           ranges = hitsRC@ranges,
+                           strand = "-")
+    hitsGRchr <- sort(c(hitsGRchr, hitsRCGRchr))
+    hitsGR <- append(hitsGR, hitsGRchr)
+  }
+  # Remove hits overlapping masked region
+  mask_hits_overlap <- findOverlaps(query = maskGR,
+                                    subject = hitsGR,
+                                    type = "any",
+                                    select = "all",
+                                    ignore.strand = TRUE)
+  hitsGR <- hitsGR[-subjectHits(mask_hits_overlap)]
+  print(hitsGR)
+  # Write as GRanges objects and in BED and GFF3 format
+  save(hitsGR,
+       file = paste0(pwmName[x], "_matchPWM_loci_in_",
+                     genomeName, "genome_", region, ".RData"))
+  hitsgff <- data.frame(chr = as.character(seqnames(hitsGR)),
+                        source = as.character(rep(".")),
+                        feature = as.character(rep(paste0(pwmName[x], "_match"))),
+                        start = as.integer(start(hitsGR)),
+                        end = as.integer(end(hitsGR)),
+                        score = as.character(rep(".")),
+                        strand = as.character(strand(hitsGR)),
+                        phase = as.character(rep(".")),
+                        attribute = as.character(rep(".")))
+  write.table(hitsgff,
+              file = paste0(gffDir, pwmName[x], "_matchPWM_loci_in_",
+                            genomeName, "genome_", region, ".gff"),
+              row.names = F, col.names = F, quote = F, sep = "\t")
+  hitsbed <- data.frame(chr = as.character(seqnames(hitsGR)),
+                        start = as.integer(start(hitsGR)-1),
+                        end = as.integer(end(hitsGR)),
+                        name = as.integer(1:length(hitsGR)),
+                        score = rep("NA", length(hitsGR)),
+                        strand = as.character(strand(hitsGR)))
+  write.table(hitsbed,
+              file = paste0(bedDir, pwmName[x], "_matchPWM_loci_in_",
+                            genomeName, "genome_", region, ".bed"), 
+              row.names = F, col.names = F, quote = F, sep = "\t")
+}, mc.cores = length(pwmList))
+
+
+# Load ASY1 peaks that overlap H2A.Z and H3K27me3 peaks
+peaks <- read.table(paste0("/home/ajt200/analysis/wheat/ASY1_CS/snakemake_ChIPseq/mapped/both/peaks/PeakRanger1.18/ranger/p0.001_q0.01/",
+                           "ASY1_CS_Rep1_ChIP_rangerPeaksGR_minuslog10Qsorted_p0.001_q0.01_noMinWidth_in_",
+                           genomeName, "genome_", region, ".gff"),
+                    header = F)
+peaksGR <- GRanges(seqnames = peaks$V1,
+                   ranges = IRanges(start = peaks$V4,
+                                    end = peaks$V5),
+                   strand = "*")
+
+# Load list of motif matches in GRanges format
+hitsGRList <- lapply(seq_along(pwmName), function(x) {
+  load(file = paste0(pwmName[x], "_matchPWM_loci_in_",
+                     genomeName, "genome_", region, ".RData"))
+})
+
+library(doParallel)
+# Change number of cores to reflect number of samples you want to process simultaneously
+registerDoParallel(cores = length(pwmList))
+print("Currently registered parallel backend name, version and cores")
+print(getDoParName())
+print(getDoParVersion())
+print(getDoParWorkers())
+
+foreach(x = 1:pwmNum) %dopar% {
+  print(x)
+  load(file = paste0(pwmName[x], "_matchPWM_loci_in_",
+                     genomeName, "genome_", region, ".RData"))
+  peaks_hits_overlap <- findOverlaps(query = peaksGR,
+                                     subject = hitsGR,
+                                     type = "any",
+                                     select = "all",
+                                     ignore.strand = TRUE)
+  hitsGR_peaksGR <- hitsGR[unique(subjectHits(peaks_hits_overlap))]
+  print(hitsGR)
+  print(hitsGR_peaksGR)
+  hitsGRseq <- DNAStringSet()
+  hitsGR_peaksGRseq <- DNAStringSet()
+  for(h in seq_along(chrs)) {
+    hitsGRseq_chr <- DNAStringSet()
+    hitsGR_peaksGRseq_chr <- DNAStringSet()
+    # Obtain sequences for each motif match and flanking flankSize-bp regions as DNAStringSet object
+    # hits not overlapping peaks
+    for(i in seq_along(hitsGR[seqnames(hitsGR) == chrs[h]])) {
+      hitsGRseq_chr <- c(hitsGRseq_chr,
+                         DNAStringSet(chrList[[h]][(start(ranges(hitsGR)[seqnames(hitsGR)
+                                                          == chrs[h]][i])-flankSize):
+                                                   (end(ranges(hitsGR)[seqnames(hitsGR)
+                                                        == chrs[h]][i])+flankSize)]))
+    }
+    hitsGRseq <- c(hitsGRseq, hitsGRseq_chr)
+    # hits overlapping peaks
+    for(i in seq_along(hitsGR_peaksGR[seqnames(hitsGR_peaksGR) == chrs[h]])) {
+      hitsGR_peaksGRseq_chr <- c(hitsGR_peaksGRseq_chr,
+                                    DNAStringSet(chrList[[h]][(start(ranges(hitsGR_peaksGR)[seqnames(hitsGR_peaksGR)
+                                                                     == chrs[h]][i])-flankSize):
+                                                              (end(ranges(hitsGR_peaksGR)[seqnames(hitsGR_peaksGR)
+                                                                   == chrs[h]][i])+flankSize)]))
+    }
+    hitsGR_peaksGRseq <- c(hitsGR_peaksGRseq, hitsGR_peaksGRseq_chr)
+  }
+  # Generate position frequency matrix (PFM)
+  hitsGRpfm <- consensusMatrix(hitsGRseq)
+  hitsGR_peaksGRpfm <- consensusMatrix(hitsGR_peaksGRseq)
+  # Convert frequencies to proportions and retain rows 1:4
+  hitsGRprm <- prop.table(hitsGRpfm, 2)[1:4,]
+  hitsGR_peaksGRprm <- prop.table(hitsGR_peaksGRpfm, 2)[1:4,]
+  # Re-order rows for stack barplot representation
+  hitsGRprm_AGTC <- rbind(hitsGRprm[1,],
+                          hitsGRprm[3,],
+                          hitsGRprm[4,],
+                          hitsGRprm[2,])
+  rownames(hitsGRprm_AGTC) <- c("A", "G", "T", "C")
+  hitsGR_peaksGRprm_AGTC <- rbind(hitsGR_peaksGRprm[1,],
+                                  hitsGR_peaksGRprm[3,],
+                                  hitsGR_peaksGRprm[4,],
+                                  hitsGR_peaksGRprm[2,])
+  rownames(hitsGR_peaksGRprm_AGTC) <- c("A", "G", "T", "C")
+  pdf(paste0(plotDir,
+             "ASY1_CS_Rep1_ChIP_peak_motif_", pwmName[x], "_",
+             paste0(strsplit(consensusString(hitsGRseq),
+                             split = "")[[1]][flankSize+1:(flankSize+1+mean(width(hitsGR))-1)],
+                    collapse = ""),
+             "_base_proportions_in_", genomeName, "genome_", region, ".pdf"),
+      height = 5, width = 10)
+  par(mfrow = c(1, 2),
+      mgp = c(2, 1, 0))
+  barplot(hitsGRprm_AGTC,
+          col = c("green", "yellow", "red", "blue"),
+          xlab = paste0("ASY1 peak motif ", pwmName[x], "_",
+                        paste0(strsplit(consensusString(hitsGRseq),
+                                        split = "")[[1]][flankSize+1:(flankSize+1+mean(width(hitsGR))-1)],
+                               collapse = ""),
+                               " matches and ", flankName, " flanks"),
+          ylab = "Proportion",
+          main = paste(c("All matches in ", genomeName, "genome ", region), sep = ""),
+          legend.text = TRUE,
+          args.legend = list(
+            x = ncol(hitsGRprm_AGTC) + 16,
+            y = 0.2,
+            bty = "n"
+          )
+         )
+  barplot(hitsGRprm_peaksGRprm_AGTC,
+          col = c("green", "yellow", "red", "blue"),
+          xlab = paste0("ASY1 peak motif ", pwmName[x], "_",
+                        paste0(strsplit(consensusString(hitsGR_peaksGRseq),
+                                        split = "")[[1]][flankSize+1:(flankSize+1+mean(width(hitsGR_peaksGR))-1)],
+                               collapse = ""),
+                               " matches and ", flankName, " flanks"),
+          ylab = "Proportion",
+          main = paste(c("Matches overlapping ASY1 peaks in ", genomeName, "genome ", region), sep = ""),
+          legend.text = TRUE,
+          args.legend = list(
+            x = ncol(hitsGRprm_peaksGRprm_AGTC) + 16,
+            y = 0.2,
+            bty = "n"
+          )
+         )
+  dev.off()
+}
+
---
+++ ASY1_CS/snakemake_ChIPseq/mapped/both/peaks/PeakRanger1.18/ranger/p0.001_q0.01/heterochromatin/motifs_ASY1_H2AZ_H3K27me3_peakOverlap_summits200bp/weeder2_bg_ranLoc_200bpseq/A/all/distribution/matchPWM_genome/matchPWM_genome_chisquare_50bpFlank.R
@@ -0,0 +1,113 @@
+# Load genomic loci that match motifs enriched at REC8 peaks and define random loci
+# Profile base composition (-log10(chi-square P)) in regions flanking these loci
+
+library(Biostrings)
+library(BSgenome.Athaliana.TAIR.TAIR9)
+library(segmentSeq)
+library(regioneR)
+#library(zoo)
+library(TTR)
+
+inDir <- "/home/meiosis/ajt200/analysis/170920_Chris_ChIP_REC8_histone/fastq_pooled/REC8/peaks/PeakRanger1.18/ranger/MYC_Rep2_input_p0.05_q0.05/REC8_HA_Rep1_peak_profiles/motifs/weeder2_bg_armranLoc_200bpseq/distribution/matchPWM_genome/"
+PFMDir <- "/home/meiosis/ajt200/analysis/170920_Chris_ChIP_REC8_histone/fastq_pooled/REC8/peaks/PeakRanger1.18/ranger/MYC_Rep2_input_p0.05_q0.05/REC8_HA_Rep1_peak_profiles/motifs/weeder2_bg_armranLoc_200bpseq/distribution/matchPWM_genome/PFM/"
+plotDir <- "/home/meiosis/ajt200/analysis/170920_Chris_ChIP_REC8_histone/fastq_pooled/REC8/peaks/PeakRanger1.18/ranger/MYC_Rep2_input_p0.05_q0.05/REC8_HA_Rep1_peak_profiles/motifs/weeder2_bg_armranLoc_200bpseq/distribution/matchPWM_genome/plots/chisquare_P/"
+
+chrs <- c("Chr1","Chr2","Chr3","Chr4","Chr5")
+chrStart <- c(1, 1, 1, 1, 1)
+chrLens <- c(30427671, 19698289, 23459830, 18585056, 26975502)
+centromeres <- c(15086045, 3607929, 13587786, 3956021, 11725024)
+pericenStart <- c(11330001, 990001, 10200001, 990001, 8890001)
+pericenEnd <- c(18480000, 7540000, 16860000, 6850000, 15650000)
+genome <- toGRanges(data.frame(chrs, chrStart, chrLens))
+seqlevels(genome) <- sub("Chr", "", seqlevels(genome))
+mask <- toGRanges(data.frame(chrs, pericenStart, pericenEnd))
+seqlevels(mask) <- sub("Chr", "", seqlevels(mask))
+
+chr1 <- Athaliana$Chr1
+chr2 <- Athaliana$Chr2
+chr3 <- Athaliana$Chr3
+chr4 <- Athaliana$Chr4
+chr5 <- Athaliana$Chr5
+chr_list <- list()
+chr_list[[1]] <- chr1
+chr_list[[2]] <- chr2
+chr_list[[3]] <- chr3
+chr_list[[4]] <- chr4
+chr_list[[5]] <- chr5
+
+num_GR <- as.numeric(system(paste0("ls -l ", inDir, "motif*_matchPWM_GRanges.RData | wc -l"), intern = T))
+
+library(doParallel)
+# Change number of cores to reflect number of samples you want to process simultaneously
+registerDoParallel(cores = num_GR)
+print("Currently registered parallel backend name, version and cores")
+print(getDoParName())
+print(getDoParVersion())
+print(getDoParWorkers())
+
+foreach(x = 1:num_GR) %dopar% {
+  print(x)
+  load(file = paste0(inDir, "motif", x, "_matchPWM_GRanges.RData"))
+  ranLoc.GRanges <- randomizeRegions(motif.GRanges, genome = genome, per.chromosome = TRUE, allow.overlaps = TRUE)
+
+  matchSeq <- DNAStringSet()
+  ranLocSeq <- DNAStringSet()
+  for(h in 1:5) {
+    matchSeq.chr <- DNAStringSet()
+    ranLocSeq.chr <- DNAStringSet()
+    # Obtain sequences for each motif match, ranLoc and flanking 50-bp regions as DNAStringSet object
+    #for(i in 1:10) {
+    for(i in 1:length(motif.GRanges[seqnames(motif.GRanges) == h])) {
+      matchSeq.chr <- c(matchSeq.chr, DNAStringSet(chr_list[[h]][(start(ranges(motif.GRanges)[seqnames(motif.GRanges) == h][i])-50):(end(ranges(motif.GRanges)[seqnames(motif.GRanges) == h][i])+50)]))
+      ranLocSeq.chr <- c(ranLocSeq.chr, DNAStringSet(chr_list[[h]][(start(ranges(ranLoc.GRanges)[seqnames(ranLoc.GRanges) == h][i])-50):(end(ranges(ranLoc.GRanges)[seqnames(ranLoc.GRanges) == h][i])+50)]))
+    }
+    matchSeq <- c(matchSeq, matchSeq.chr)
+    ranLocSeq <- c(ranLocSeq, ranLocSeq.chr)
+  }
+  # Generate position frequency matrix (PFM) for each sequence
+  matchSeqPFM <- consensusMatrix(matchSeq)
+  ranLocSeqPFM <- consensusMatrix(ranLocSeq)
+
+  write.table(matchSeqPFM, file = paste0(PFMDir, "motif", x, "_matchSeqPFM_50bpFlank.txt"))
+  write.table(ranLocSeqPFM, file = paste0(PFMDir, "motif", x, "_ranLocSeqPFM_50bpFlank.txt"))
+
+  #X2 = Σ (observed – expected)2 / expected
+
+  matchMaxBases <- NULL
+  matchMaxFreq <- NULL
+  matchSumFreq <- NULL
+  ranLocBases <- NULL
+  ranLocFreq <- NULL
+  ranLocSumFreq <- NULL
+  for(i in 1:dim(matchSeqPFM)[2]) {
+    matchMaxBases <- c(matchMaxBases, names(matchSeqPFM[,i][matchSeqPFM[,i] == max(matchSeqPFM[,i])]))
+    ranLocBases <- c(ranLocBases, names(ranLocSeqPFM[,i][matchSeqPFM[,i] == max(matchSeqPFM[,i])]))
+    matchMaxFreq <- c(matchMaxFreq, as.integer(matchSeqPFM[,i][matchSeqPFM[,i] == max(matchSeqPFM[,i])]))
+    ranLocFreq <- c(ranLocFreq, as.integer(ranLocSeqPFM[,i][matchSeqPFM[,i] == max(matchSeqPFM[,i])]))
+    matchSumFreq <- c(matchSumFreq, sum(matchSeqPFM[,i]))
+    ranLocSumFreq <- c(ranLocSumFreq, sum(ranLocSeqPFM[,i]))
+  }
+ 
+  observed <- NULL
+  expectedProportions <- NULL
+  for(j in 1:length(matchMaxFreq)) {
+    observed <- rbind(observed, c(matchMaxFreq[j], matchSumFreq[j]-matchMaxFreq[j]))
+    expectedProportions <- rbind(expectedProportions,
+                                 c(ranLocFreq[j]/ranLocSumFreq[j],
+                                  (ranLocSumFreq[j]-ranLocFreq[j])/ranLocSumFreq[j]))
+  }
+
+  chisqList <- lapply(seq_along(1:dim(observed)[1]), function(k) {
+    chisq.test(observed[k,1:2], p = expectedProportions[k,1:2])
+  })
+  save(chisqList, file = paste0(plotDir, "motif", x, "_chisqList_50bpFlank.RData"))
+
+  chisqMatrix <- NULL
+  for(k in 1:dim(observed)[1]) {
+    chisq <- chisq.test(observed[k,1:2], p = expectedProportions[k,1:2])
+    chisqMatrix <- rbind(chisq, chisqMatrix)
+  }
+  chisqDataFrame <- as.data.frame(chisqMatrix, row.names = as.character(1:18))
+  save(chisqDataFrame, file = paste0(plotDir, "motif", x, "_chisqDataFrame_50bpFlank.RData"))
+}
+
