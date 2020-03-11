0555bff2966bff2397f4183104a9f5ef788c4711
ajtock <andytock@gmail.com>
2020-03-11 20:41:25 +0000

    hypergeomTests

--- ASY1_CS/snakemake_ChIPseq/mapped/geneProfiles_subgenomes/quantiles/proportion_NLRs_in_gene_quantiles_hypergeometricTest.R
+++ ASY1_CS/snakemake_ChIPseq/mapped/geneProfiles_subgenomes/quantiles/proportion_NLRs_in_gene_quantiles_hypergeometricTest.R
@@ -20,12 +20,20 @@
 # length(TAIR10_TEfamIDs) [m] + (length(TAIR10_TEIDs)-length(TAIR10_TEfamIDs)) [n]
 
 # Usage 
-# ./proportion_NLRs_in_gene_quantiles_hypergeometricTest.R 'ASY1_in_bodies' 'ASY1_CS_Rep1_ChIP' 1 4 'genomewide' 'Agenome_Bgenome_Dgenome' 100000
+# ./proportion_NLRs_in_gene_quantiles_hypergeometricTest.R 'ASY1_CS_Rep1_ChIP' 'bodies' 1 4 'genomewide' 'Agenome_Bgenome_Dgenome' 100000
 
 library(methods)
+library(plotrix)
+library(ggplot2)
+library(ggbeeswarm)
+library(ggthemes)
+library(grid)
+library(gridExtra)
+library(extrafont)
 
-#quantileBy <- "ASY1_in_bodies"
+
 #libName <- "ASY1_CS_Rep1_ChIP"
+#featRegion <- "bodies"
 #quantileFirst <- 1
 #quantileLast <- 4
 #region <- "genomewide"
@@ -33,18 +41,44 @@ library(methods)
 #samplesNum <- 100000
 
 args <- commandArgs(trailingOnly = TRUE)
-quantileBy <- args[1]
-libName <- args[2]
+libName <- args[1]
+featRegion <- args[2]
 quantileFirst <- as.integer(args[3])
 quantileLast <- as.integer(args[4])
 region <- args[5]
 genomeName <- args[6]
 samplesNum <- as.numeric(args[7])
 
-outDir <- paste0("quantiles_by_", quantileBy, "/hypergeometricTests/")
+outDir <- paste0("quantiles_by_", sub("_\\w+$", "", libName), "_in_", featRegion, "/hypergeometricTests/")
 plotDir <- paste0(outDir, "plots/")
 system(paste0("[ -d ", outDir, " ] || mkdir ", outDir))
 system(paste0("[ -d ", plotDir, " ] || mkdir ", plotDir))
+
+# Define quantile colours
+quantileColours <- c("red", "purple", "blue", "navy")
+makeTransparent <- function(thisColour, alpha = 180)
+{
+  newColour <- col2rgb(thisColour)
+  apply(newColour, 2, function(x) {
+    rgb(red = x[1], green = x[2], blue = x[3],
+        alpha = alpha, maxColorValue = 255)
+  })
+}
+quantileColours <- makeTransparent(quantileColours)
+
+# Load feature quantiles
+featuresDF <- read.table(paste0("quantiles_by_", sub("_\\w+$", "", libName), "_in_", featRegion,
+                                "/WesternEurope/features_", quantileLast, "quantiles_by_",
+                                sub("_\\w+$", "", libName), "_in_", featRegion, "_of_genes_in_",
+                                genomeName, "_", region, "_WesternEurope.txt"),
+                         header = T, sep = "\t", row.names = NULL, stringsAsFactors = F)
+featuresDF$featureID <- sub(pattern = "\\.\\d+", replacement = "",
+                            x = featuresDF$featureID)
+genome_genes <- featuresDF$featureID
+quantile_genes_list <- lapply(quantileFirst:quantileLast, function(x) {
+  featuresDF[featuresDF$quantile == paste0("Quantile ", x),]$featureID
+})
+rm(featuresDF); gc()
 
 # Load NLRs
 NLRs <- read.table("/home/ajt200/analysis/wheat/annotation/221118_download/iwgsc_refseqv1.1_genes_2017July06/NLRs_Krasileva/NB_ARC_genes_IWGSC_v1_Ksenia_Krasileva_representative_mRNA.gff3",
@@ -60,22 +94,9 @@ genome_NLRs <- as.character(NLRs$V9)
 #NLRs <- read.table("/home/ajt200/analysis/wheat/RNAseq_meiocyte_Alabdullah_Moore_2019_FrontPlantSci/Table_S4_meiotic_gene_orthologs.tsv",
 #                   header = T, stringsAsFactors = F)
 #genome_NLRs <- as.character(NLRs$Gene.ID)
-
-featuresDF <- read.table(paste0("quantiles_by_", quantileBy,
-                                "/WesternEurope/features_", quantileLast, "quantiles_by_",
-                                quantileBy, "_of_genes_in_",
-                                genomeName, "_", region, "_WesternEurope.txt"),
-                         header = T, sep = "\t", row.names = NULL, stringsAsFactors = F)
-featuresDF$featureID <- sub(pattern = "\\.\\d+", replacement = "",
-                            x = featuresDF$featureID)
-genome_genes <- featuresDF$featureID
-quantile_genes_list <- lapply(quantileFirst:quantileLast, function(x) {
-  featuresDF[featuresDF$quantile == paste0("Quantile ", x),]$featureID
-})
-rm(featuresDF); gc()
-
 # Get the intersection of genome_NLRs and genome_genes
 # (this excludes NLRs not assigned to a chromosome)
+
 genome_NLRs <- intersect(genome_NLRs, genome_genes)
 
 # Set class for hypergeometric test results object
@@ -162,77 +183,83 @@ for(z in seq_along(quantile_genes_list)) {
                        hypergeomDist = hgDist)
   save(hgTestResults,
        file = paste0(outDir,
-                     orderingFactor, "_", pop_name[x], "_random_nonIDs_permTest_for_", gsub(" ", "_", featureNamePl
-ot),
-                     "_in_quantile", quantileNo, "_of_", quantiles,
-                     "_by_log2_", libName, "_control_in_", region, "_of_",
-                     substring(featureName[1][1], first = 1, last = 5), "_in_",
-                     paste0(substring(featureName, first = 10, last = 16),
-                            collapse = "_"), "_",
-                     substring(featureName[1][1], first = 18),
-                     ".RData"))
+                     "NLR_gene_representation_among_quantile", z, "_of_", quantileLast,
+                     "_by_log2_", libName, "_control_in_", featRegion, "_of_genes_in_",
+                     genomeName, "_", region, "_hypergeomTestRes.RData"))
 
-
-  save(hgTestResults,
-       file = paste0(outDir,
-                     baseName,
-                     "_hypergeometricTestResults_",
-                     TEfamNames[x],
-                     ".RData"))
-
-  library(plotrix)
   # Generate histogram
   pdf(paste0(plotDir,
-             "hist_",
-             baseName,
-             "_hypergeometricTestResults_",
-             TEfamNames[x],
-             ".pdf"),
-             height = 4, width = 5)
+             "NLR_gene_representation_among_quantile", z, "_of_", quantileLast,
+             "_by_log2_", libName, "_control_in_", featRegion, "_of_genes_in_",
+             genomeName, "_", region, "_hypergeomTestRes_hist.pdf"),
+             height = 4.5, width = 5)
+  par(mar = c(3.1, 3.1, 4.1, 1.1),
+      mgp = c(1.85, 0.75, 0))
   ## Disable scientific notation (e.g., 0.0001 rather than 1e-04)
   #options(scipen = 100)
   # Calculate max density
-  maxDensityPlus <- max(density(hgTestResults@random_proportions_DE_TEfam)$y)*1.2
-  if(hgTestResults@proportion_DE_TEfam > mean(hgTestResults@random_proportions_DE_TEfam)) {
-    alpha0.05 <- quantile(hgTestResults@random_proportions_DE_TEfam, 0.95)[[1]]
-    xlim <- c(pmax(0, min(hgTestResults@random_proportions_DE_TEfam)-.1),
-              pmax(hgTestResults@proportion_DE_TEfam+.1, alpha0.05+.1))
-    Pval <- Pval_overrep
+  maxDensityPlus <- max(density(hgTestResults@hypergeomDist)$y)*1.2
+  if(hgTestResults@alternative == "MoreThanRandom") {
+    xlim <- c(pmin(0, min(hgTestResults@hypergeomDist)/1.2),
+              pmax(hgTestResults@observed*1.2, hgTestResults@alpha0.05*1.2))
+    textX1 <- quantile(xlim, 0.25)[[1]]
+#    textX1 <- min(hgTestResults@hypergeomDist)/1.15
   } else {
-    alpha0.05 <- quantile(hgTestResults@random_proportions_DE_TEfam, 0.05)[[1]]
-    xlim <- c(pmax(0, pmin(hgTestResults@proportion_DE_TEfam-.1, alpha0.05-.1)),
-              max(hgTestResults@random_proportions_DE_TEfam)+.1)
-    Pval <- Pval_underrep
+    xlim <- c(pmin(0, hgTestResults@observed/1.2),
+              max(hgTestResults@hypergeomDist)*1.2)
+    textX1 <- quantile(xlim, 0.75)[[1]]
+#    textX1 <- min(hgTestResults@hypergeomDist)/1.15
   }
-  par(mar = c(5.1, 4.1, 4.1, 2.1))
-  hist(hgTestResults@random_proportions_DE_TEfam,
+  hist(hgTestResults@hypergeomDist,
+       breaks = 50,
        freq = FALSE,
-       col = "grey70",
+       col = "dodgerblue",
        border = NA,
        lwd = 2,
-       xlim = xlim,
+       xlim = c(pretty(xlim)[1],
+                pretty(xlim)[length(pretty(xlim))]),
        ylim = c(0,
                 maxDensityPlus),
-       xlab = paste0("Proportion of ", regulated, " transposable elements \n that are ",
-                     TEfamNamesPlot[x], " elements"),
-       ylab = "Density",
-       main = "",
-       cex.lab = 1, cex.axis = 1)
-  titleText <- list(bquote(.(baseName)),
-                    bquote(italic("P")~" = "~.(Pval)),
-                    bquote("Samples (hypergeometric distribution) = "~.(samplesChar)))
-  mtext(do.call(expression, titleText), side = 3, line = 2:0, cex = 1)
-  lines(density(hgTestResults@random_proportions_DE_TEfam), lwd = 1.5)
-  ablineclip(v = mean(hgTestResults@random_proportions_DE_TEfam),
+       xaxt = "n", yaxt = "n",
+       xlab = "", ylab = "", main = "",
+       axes = FALSE)
+  axis(side = 2,
+       at = pretty(density(hgTestResults@hypergeomDist)$y),
+       lwd = 2)
+  mtext(side = 2,
+        text = "Density",
+        line = 1.85)
+  axis(side = 1,
+       at = pretty(xlim),
+       lwd = 2)
+  mtext(side = 1,
+        text = bquote("Genes"),
+        line = 1.85)
+  titleText <- list(bquote("NLR-encoding genes in" ~
+                           .(sub("_\\w+$", "", libName)) ~ "Quantile" ~ .(as.character(z)) ~
+                           "(" * .(featRegion) * ") in" ~
+                           .(gsub("_", " ", genomeName)) ~ .(region)),
+                    bquote(italic("P")*" = "*
+#                           .(as.character(round(hgTestResults@pval,
+#                                                digits = 6)))),
+                           .(as.character(hgTestResults@pval))),
+                    bquote("Samples (hypergeometric distribution) = "*.(prettyNum(length(hgTestResults@hypergeomDist),
+                                                                                  big.mark = ",",
+                                                                                  trim = T))))
+  mtext(do.call(expression, titleText), side = 3, line = 3:1, cex = c(0.7, 1, 1))
+  lines(density(hgTestResults@hypergeomDist),
+        col = "dodgerblue3",
+        lwd = 1.5)
+  ablineclip(v = hgTestResults@expected,
              y1 = 0, y2 = maxDensityPlus*.92, lwd = 2)
-  ablineclip(v = hgTestResults@proportion_DE_TEfam,
+  ablineclip(v = hgTestResults@observed,
              y1 = 0, y2 = maxDensityPlus*.92, lwd = 2, col = "forestgreen")
-  ablineclip(v = alpha0.05,
+  ablineclip(v = hgTestResults@alpha0.05,
              y1 = 0, y2 = maxDensityPlus*.92, lwd = 2, lty = 5, col = "red")
-  text(x = c(pmax(0.05, min(hgTestResults@random_proportions_DE_TEfam)-.05),
-             mean(hgTestResults@random_proportions_DE_TEfam),
-             hgTestResults@proportion_DE_TEfam,
-             alpha0.05),
+  text(x = c(textX1,
+             hgTestResults@expected,
+             hgTestResults@observed,
+             hgTestResults@alpha0.05),
        y = c(maxDensityPlus*.95,
              maxDensityPlus,
              maxDensityPlus,
@@ -240,13 +267,83 @@ ot),
        labels = c("Simulated",
                   "Expected",
                   "Observed",
-                  expression(alpha~" = 0.05")),
-       col = c("grey70",
+                  expression(alpha*" = 0.05")),
+       col = c("dodgerblue",
                "black",
                "forestgreen",
                "red"),
-       cex = 0.7)
-  box(lwd = 2)
+       cex = 0.8)
   dev.off()
+}
 
-})
+
+options(scipen = 100)
+
+# Plot bar graph summarising permutation test results
+pt_list <- list()
+for(z in quantileFirst:quantileLast) {
+  load(
+       paste0(outDir,
+              "NLR_gene_representation_among_quantile", z, "_of_", quantileLast,
+              "_by_log2_", libName, "_control_in_", featRegion, "_of_genes_in_",
+              genomeName, "_", region, "_hypergeomTestRes.RData"))
+  pt_list <- c(pt_list, hgTestResults)
+}
+bargraph_df <- data.frame(Quantile = paste0("Quantile ", quantileFirst:quantileLast),
+                          log2ObsExp = sapply(seq_along(pt_list), function(x) { pt_list[[x]]@log2obsexp }),
+                          log2alpha0.05 = sapply(seq_along(pt_list), function(x) { pt_list[[x]]@log2alpha }))
+bargraph_df$Quantile <- factor(bargraph_df$Quantile,
+                               levels = paste0("Quantile ", quantileFirst:quantileLast))
+bp <- ggplot(data = bargraph_df,
+             mapping = aes(x = Quantile,
+                           y = log2ObsExp,
+                           fill = " ")) +
+  geom_bar(stat = "identity",
+           position = position_dodge()) +
+  scale_fill_manual(name = "",
+                    values = c("dodgerblue3"),
+                    labels = " ") +
+  geom_point(mapping = aes(x = Quantile,
+                           y = log2alpha0.05),
+             position = position_dodge(0.9),
+             shape = "-", colour  = "grey80", size = 20) +
+  labs(y = bquote("Log"[2]*"(observed/expected) genes in quantile")) +
+#  scale_y_continuous(limits = c(-1.5, 1.5)) +
+  scale_x_discrete(position = "top") +
+  guides(fill = guide_legend(direction = "horizontal",
+                             label.position = "top",
+                             label.theme = element_text(size = 20, hjust = 0, vjust = 0.5, angle = 90),
+                             nrow = 1,
+                             byrow = TRUE)) +
+  theme_bw() +
+  theme(axis.line.y = element_line(size = 1, colour = "black"),
+        axis.ticks.y = element_line(size = 1, colour = "black"),
+        axis.text.y = element_text(size = 20, colour = "black", hjust = 0.5, vjust = 0.5, angle = 90),
+        axis.title.y = element_text(size = 20, colour = "black"),
+        axis.ticks.x = element_blank(),
+        axis.text.x = element_text(size = 20, colour = "black", hjust = 0, vjust = 0.5, angle = 90),
+        axis.title.x = element_blank(),
+        panel.grid = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank(),
+        legend.position = "none",
+        #legend.position = c(0.05, 0.30),
+        legend.background = element_rect(fill = "transparent"),
+        legend.key = element_rect(colour = "transparent",
+                                  fill = "transparent"),
+        plot.margin = unit(c(5.5, 5.5, 10.5, 5.5), "pt"),
+        plot.title = element_text(size = 16, colour = "black", hjust = 0.5)) +
+  ggtitle(bquote("NLR-encoding genes in" ~
+                 .(sub("_\\w+$", "", libName)) ~ "quantiles" ~
+                 "(" * .(featRegion) * ") in" ~
+                 .(gsub("_", " ", genomeName)) ~ .(region) ~
+                 "(" * .(prettyNum(samplesNum,
+                                   big.mark = ",",
+                                   trim = T)) ~ " samples)"))
+ggsave(paste0(plotDir,
+              "bargraph_NLR_gene_representation_among_", quantileLast,
+              "quantiles_by_log2_", libName, "_control_in_", featRegion, "_of_genes_in_",
+              genomeName, "_", region, "_hypergeomTestRes.pdf"),
+       plot = bp,
+       height = 8, width = 12)
+
