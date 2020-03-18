52b0b43ef9eb80b7225f3adfe6d7c6903b474158
ajtock <andytock@gmail.com>
2020-03-18 19:48:46 +0000

    quantile genes overlapping MIRs and LARs

--- ASY1_CS/snakemake_ChIPseq/mapped/geneProfiles_subgenomes/quantiles/proportion_overlapping_local_adaptation_regions_in_gene_quantiles_hypergeometricTest.R
+++ ASY1_CS/snakemake_ChIPseq/mapped/geneProfiles_subgenomes/quantiles/proportion_overlapping_local_adaptation_regions_in_gene_quantiles_hypergeometricTest.R
@@ -2,15 +2,15 @@
 
 # Perform hypergeometric tests to determine whether each ASY1 or DMC1
 # gene quantile is over-represented or under-represented for
-# genes overlapping selective sweep regions associated with modern wheat improvement
-# (MIR-overlapping genes) (regions as identified by He et al. 2019 Nat. Genet. 51)
-# (i.e., is the propotion of MIR-overlapping genes
+# genes overlapping selective sweep regions associated with local_adaptation
+# (LAR-overlapping genes) (regions as identified by He et al. 2019 Nat. Genet. 51)
+# (i.e., is the propotion of LAR-overlapping genes
 # contained within each gene quantile significantly greater or smaller than expected by chance
 # based on the hypergeometric distribution?)
 
-# P-value is the probability of drawing >= length(quantile_MIRs) [x] features
+# P-value is the probability of drawing >= length(quantile_LARs) [x] features
 # in a sample size of length(quantile_genes) [k] from a total feature set consisting of
-# length(genome_MIRs) [m] + ( length(genome_genes) - length(genome_MIRs)) [n]
+# length(genome_LARs) [m] + ( length(genome_genes) - length(genome_LARs)) [n]
 
 ### Perform hypergeometric test to determine whether a significant proportion of 
 ### elements belonging to each transposon family are differentially expressed in kyp suvh5 suvh6
@@ -20,7 +20,7 @@
 # length(TAIR10_TEfamIDs) [m] + (length(TAIR10_TEIDs)-length(TAIR10_TEfamIDs)) [n]
 
 # Usage 
-# ./proportion_overlapping_improvement_regions_in_gene_quantiles_hypergeometricTest.R 'ASY1_CS_Rep1_ChIP' 'bodies' 1 4 'genomewide' 'Agenome_Bgenome_Dgenome' 100000
+# ./proportion_overlapping_local_adaptation_regions_in_gene_quantiles_hypergeometricTest.R 'ASY1_CS_Rep1_ChIP' 'bodies' 1 4 'genomewide' 'Agenome_Bgenome_Dgenome' 100000
 
 library(methods)
 library(plotrix)
@@ -96,29 +96,29 @@ quantile_genes_list <- lapply(quantileFirst:quantileLast, function(x) {
 })
 rm(featuresDF); gc()
 
-# Load selective sweep regions assocaited with modern wheat improvement (MIRs)
-MIRs <- read.table(paste0("/home/ajt200/analysis/wheat/annotation/221118_download/He_Akhunov_2019_NatGenet_1000exomes_SNPs/",
-                          "Table_S11_41588_2019_382_MOESM11_ESM.tsv"),
+# Load selective sweep regions assocaited with local_adaptation (LARs)
+LARs <- read.table(paste0("/home/ajt200/analysis/wheat/annotation/221118_download/He_Akhunov_2019_NatGenet_1000exomes_SNPs/",
+                          "Table_S10_41588_2019_382_MOESM10_ESM.tsv"),
                    header = T, sep = "\t", stringsAsFactors = F)
-colnames(MIRs) <- c("chr", "start", "end", "populations", "num_populations")
+colnames(LARs) <- c("chr", "start", "end", "num_top_SNPs", "env_vars", "num_env_vars")
 chrs <- paste0(rep("chr", 21), rep(1:7, 3),
                c(rep("A", 7), rep("B", 7), rep("D", 7)))
 genomeLetter <- unlist(strsplit(gsub("genome", "", genomeName), split = "_"))
-# Subset MIRs to only those within a given subgenome
+# Subset LARs to only those within a given subgenome
 if(length(genomeLetter) == 1) {
   chrs <- chrs[grepl(genomeLetter, chrs)]
-  MIRs <- MIRs[MIRs$chr %in% chrs,]
+  LARs <- LARs[LARs$chr %in% chrs,]
 }
-MIRs <- GRanges(seqnames = MIRs$chr,
-                ranges = IRanges(start = MIRs$start,
-                                 end = MIRs$end),
+LARs <- GRanges(seqnames = LARs$chr,
+                ranges = IRanges(start = LARs$start,
+                                 end = LARs$end),
                 strand = "*")
-MIRs_genome_genes_overlap <- findOverlaps(query = MIRs,
+LARs_genome_genes_overlap <- findOverlaps(query = LARs,
                                           subject = genome_genes,
                                           type = "any",
                                           select = "all",
                                           ignore.strand = T)
-genome_MIRs <- genome_genes[unique(subjectHits(MIRs_genome_genes_overlap))]
+genome_LARs <- genome_genes[unique(subjectHits(LARs_genome_genes_overlap))]
 
 # Set class for hypergeometric test results object
 setClass("hypergeomTest",
@@ -134,55 +134,55 @@ setClass("hypergeomTest",
                         random_proportions_of_quantile = "numeric",
                         hypergeomDist = "numeric"))
 
-# P-value is the probability of drawing >= length(quantile_MIRs) [x] features
+# P-value is the probability of drawing >= length(quantile_LARs) [x] features
 # in a sample size of length(quantile_genes) [k] from a total feature set consisting of
-# length(genome_MIRs) [m] + ( length(genome_genes) - length(genome_MIRs)) [n] ,
-# where quantile_MIRs are MIR-overlapping genes in a given quantile,
-# and genome_MIRs are MIR-overlapping genes across all quantiles
+# length(genome_LARs) [m] + ( length(genome_genes) - length(genome_LARs)) [n] ,
+# where quantile_LARs are LAR-overlapping genes in a given quantile,
+# and genome_LARs are LAR-overlapping genes across all quantiles
 
 # From Karl Broman's answer at
 # https://stats.stackexchange.com/questions/16247/calculating-the-probability-of-gene-list-overlap-between-an-rna-seq-and-a-chip-c:
 # dhyper(x, m, n, k) gives the probability of drawing exactly x.
 # So P-value is given by the sum of the probabilities of drawing
-# length(quantile_MIRs) to length(quantile_genes)
+# length(quantile_LARs) to length(quantile_genes)
 
 #lapply(seq_along(quantile_genes_list), function(z) {
 for(z in seq_along(quantile_genes_list)) {
   quantile_genes <- quantile_genes_list[[z]]
-  # Within the given quantile, get genes that overlap MIRs
-  quantile_MIRs <- genome_MIRs[genome_MIRs$quantile == paste0("Quantile ", z)]
+  # Within the given quantile, get genes that overlap LARs
+  quantile_LARs <- genome_LARs[genome_LARs$quantile == paste0("Quantile ", z)]
 
   # Calculate the P-values for over-representation and under-representation
-  # of MIRs among quantile z genes
+  # of LARs among quantile z genes
   set.seed(2847502)
   # Over-representation:
-  Pval_overrep <- sum(dhyper(x = length(quantile_MIRs):length(quantile_genes),
-                             m = length(genome_MIRs),
-                             n = length(genome_genes) - length(genome_MIRs),
+  Pval_overrep <- sum(dhyper(x = length(quantile_LARs):length(quantile_genes),
+                             m = length(genome_LARs),
+                             n = length(genome_genes) - length(genome_LARs),
                              k = length(quantile_genes)))
   print(Pval_overrep)
 
-  # Or by 1 minus the sum of the probabilities of drawing 0:(length(quantile_MIRs)-1)
-  print(1 - sum(dhyper(x = 0:(length(quantile_MIRs)-1),
-                       m = length(genome_MIRs),
-                       n = length(genome_genes) - length(genome_MIRs),
+  # Or by 1 minus the sum of the probabilities of drawing 0:(length(quantile_LARs)-1)
+  print(1 - sum(dhyper(x = 0:(length(quantile_LARs)-1),
+                       m = length(genome_LARs),
+                       n = length(genome_genes) - length(genome_LARs),
                        k = length(quantile_genes))))
 
   # Under-representation
-  Pval_underrep <- phyper(q = length(quantile_MIRs),
-                          m = length(genome_MIRs),
-                          n = length(genome_genes) - length(genome_MIRs),
+  Pval_underrep <- phyper(q = length(quantile_LARs),
+                          m = length(genome_LARs),
+                          n = length(genome_genes) - length(genome_LARs),
                           k = length(quantile_genes))
   print(Pval_underrep)
 
   # Sample without replacement
   hgDist <- rhyper(nn = samplesNum,
-                   m = length(genome_MIRs),
-                   n = length(genome_genes) - length(genome_MIRs),
+                   m = length(genome_LARs),
+                   n = length(genome_genes) - length(genome_LARs),
                    k = length(quantile_genes))
 
   # Calculate P-values and significance levels
-  if(length(quantile_MIRs) > mean(hgDist)) {
+  if(length(quantile_LARs) > mean(hgDist)) {
     Pval <- Pval_overrep
     MoreOrLessThanRandom <- "MoreThanRandom"
     alpha0.05 <- quantile(hgDist, probs = 0.95)[[1]]
@@ -196,24 +196,24 @@ for(z in seq_along(quantile_genes_list)) {
                        alternative = MoreOrLessThanRandom,
                        alpha0.05 = alpha0.05,
                        pval = Pval,
-                       observed = length(quantile_MIRs),
+                       observed = length(quantile_LARs),
                        expected = mean(hgDist),
-                       log2obsexp = log2( length(quantile_MIRs) / mean(hgDist) ),
+                       log2obsexp = log2( length(quantile_LARs) / mean(hgDist) ),
                        log2alpha  = log2( alpha0.05 / mean(hgDist) ),
                        quantile_genes = length(quantile_genes),
-                       proportion_of_quantile = length(quantile_MIRs) / length(quantile_genes),
+                       proportion_of_quantile = length(quantile_LARs) / length(quantile_genes),
                        random_proportions_of_quantile = hgDist / length(quantile_genes),
                        hypergeomDist = hgDist)
   if(libName %in% "cMMb") {
   save(hgTestResults,
        file = paste0(outDir,
-                     "MIR_overlapping_gene_representation_among_quantile", z, "_of_", quantileLast,
+                     "LAR_overlapping_gene_representation_among_quantile", z, "_of_", quantileLast,
                      "_by_", libName, "_of_genes_in_",
                      genomeName, "_", region, "_hypergeomTestRes.RData"))
   } else {
   save(hgTestResults,
        file = paste0(outDir,
-                     "MIR_overlapping_gene_representation_among_quantile", z, "_of_", quantileLast,
+                     "LAR_overlapping_gene_representation_among_quantile", z, "_of_", quantileLast,
                      "_by_log2_", libName, "_control_in_", featRegion, "_of_genes_in_",
                      genomeName, "_", region, "_hypergeomTestRes.RData"))
   }
@@ -221,13 +221,13 @@ for(z in seq_along(quantile_genes_list)) {
   # Generate histogram
   if(libName %in% "cMMb") {
   pdf(paste0(plotDir,
-             "MIR_overlapping_gene_representation_among_quantile", z, "_of_", quantileLast,
+             "LAR_overlapping_gene_representation_among_quantile", z, "_of_", quantileLast,
              "_by_", libName, "_of_genes_in_",
              genomeName, "_", region, "_hypergeomTestRes_hist.pdf"),
              height = 4.5, width = 5)
   } else {
   pdf(paste0(plotDir,
-             "MIR_overlapping_gene_representation_among_quantile", z, "_of_", quantileLast,
+             "LAR_overlapping_gene_representation_among_quantile", z, "_of_", quantileLast,
              "_by_log2_", libName, "_control_in_", featRegion, "_of_genes_in_",
              genomeName, "_", region, "_hypergeomTestRes_hist.pdf"),
              height = 4.5, width = 5)
@@ -274,7 +274,7 @@ for(z in seq_along(quantile_genes_list)) {
   mtext(side = 1,
         text = bquote("Genes"),
         line = 1.85)
-  titleText <- list(bquote("MIR-overlapping genes in" ~
+  titleText <- list(bquote("LAR-overlapping genes in" ~
                            .(sub("_\\w+$", "", libName)) ~ "Quantile" ~ .(as.character(z)) ~
                            "(" * .(featRegion) * ") in" ~
                            .(gsub("_", " ", genomeName)) ~ .(region)),
@@ -323,12 +323,12 @@ pt_list <- list()
 for(z in quantileFirst:quantileLast) {
   if(libName %in% "cMMb") {
   load(paste0(outDir,
-              "MIR_overlapping_gene_representation_among_quantile", z, "_of_", quantileLast,
+              "LAR_overlapping_gene_representation_among_quantile", z, "_of_", quantileLast,
               "_by_", libName, "_of_genes_in_",
               genomeName, "_", region, "_hypergeomTestRes.RData"))
   } else {
   load(paste0(outDir,
-              "MIR_overlapping_gene_representation_among_quantile", z, "_of_", quantileLast,
+              "LAR_overlapping_gene_representation_among_quantile", z, "_of_", quantileLast,
               "_by_log2_", libName, "_control_in_", featRegion, "_of_genes_in_",
               genomeName, "_", region, "_hypergeomTestRes.RData"))
   }
@@ -378,7 +378,7 @@ bp <- ggplot(data = bargraph_df,
                                   fill = "transparent"),
         plot.margin = unit(c(5.5, 5.5, 10.5, 5.5), "pt"),
         plot.title = element_text(size = 16, colour = "black", hjust = 0.5)) +
-  ggtitle(bquote("MIR-overlapping genes in" ~
+  ggtitle(bquote("LAR-overlapping genes in" ~
                  .(sub("_\\w+$", "", libName)) ~ "quantiles" ~
                  "(" * .(featRegion) * ") in" ~
                  .(gsub("_", " ", genomeName)) ~ .(region) ~
@@ -387,14 +387,14 @@ bp <- ggplot(data = bargraph_df,
                                    trim = T)) ~ " samples)"))
 if(libName %in% "cMMb") {
 ggsave(paste0(plotDir,
-              "bargraph_MIR_overlapping_gene_representation_among_", quantileLast,
+              "bargraph_LAR_overlapping_gene_representation_among_", quantileLast,
               "quantiles_by_", libName, "_of_genes_in_",
               genomeName, "_", region, "_hypergeomTestRes.pdf"),
        plot = bp,
        height = 8, width = 12)
 } else {
 ggsave(paste0(plotDir,
-              "bargraph_MIR_overlapping_gene_representation_among_", quantileLast,
+              "bargraph_LAR_overlapping_gene_representation_among_", quantileLast,
               "quantiles_by_log2_", libName, "_control_in_", featRegion, "_of_genes_in_",
               genomeName, "_", region, "_hypergeomTestRes.pdf"),
        plot = bp,
