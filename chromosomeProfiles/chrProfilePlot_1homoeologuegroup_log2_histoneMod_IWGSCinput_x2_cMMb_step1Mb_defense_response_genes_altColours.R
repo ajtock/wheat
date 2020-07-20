6d922c2cfcd5667dfdd1ca3ccb38965e6266ae6d
ajtock <andytock@gmail.com>
2020-07-20 20:43:37 +0100

    chrProfiles update

--- chromosomeProfiles/chrProfilePlot_1homoeologuegroup_log2_histoneMod_IWGSCinput_x2_cMMb_step1Mb_defense_response_genes_altColours.R
+++ chromosomeProfiles/chrProfilePlot_1homoeologuegroup_log2_histoneMod_IWGSCinput_x2_cMMb_step1Mb_defense_response_genes_altColours.R
@@ -2,12 +2,12 @@
 
 # Plot smoothed library-size-normalized coverage in windows along chromosomes
 
-# Change xblocks height to 0.62 in chrPartitionPlotCov2_feature2 function
+# Change xblocks height to 7.0 in chrPartitionPlotCov2_feature2 function
 
 # Usage:
-# ./chrProfilePlot_1homoeologuegroup_log2_histoneMod_IWGSCinput_x2_cMMb_step1Mb_defense_response_genes_altColours.R CENH3 CENH3_ChIP_SRR1686799 DMC1 DMC1_Rep1_ChIP input H3_input_SRR6350669 input H3_input_SRR6350669 both 1Mb 1000000 15 dodgerblue limegreen 190620 'Gypsy_LTR_RLG_subfamily_RLG_famc8.3' 'chr3A,chr3B,chr3D'
-# ./chrProfilePlot_1homoeologuegroup_log2_histoneMod_IWGSCinput_x2_cMMb_step1Mb_defense_response_genes_altColours.R H3K27me1 H3K27me1_Rep1_ChIP H3K27me3 H3K27me3_ChIP_SRR6350666 MNase MNase_Rep1 input H3_input_SRR6350669 both 1Mb 1000000 15 navy firebrick1 190620 'Gypsy_LTR_RLG_subfamily_RLG_famc8.3' 'chr3A,chr3B,chr3D'
-# ./chrProfilePlot_1homoeologuegroup_log2_histoneMod_IWGSCinput_x2_cMMb_step1Mb_defense_response_genes_altColours.R H3K4me1 H3K4me1_Rep1_ChIP_SRR8126618 H3K27ac H3K27ac_Rep1_ChIP_SRR8126621 input H3_input_SRR6350669 input H3_input_SRR6350669 both 1Mb 1000000 15 magenta3 goldenrod1 190620 'Gypsy_LTR_RLG_subfamily_RLG_famc8.3' 'chr3A,chr3B,chr3D'
+# ./chrProfilePlot_1homoeologuegroup_log2_histoneMod_IWGSCinput_x2_cMMb_step1Mb_defense_response_genes_altColours.R CENH3 CENH3_ChIP_SRR1686799 DMC1 DMC1_Rep1_ChIP input H3_input_SRR6350669 input H3_input_SRR6350669 both 1Mb 1000000 15 dodgerblue limegreen 200720 'Gypsy_LTR_RLG_subfamily_RLG_famc8.3' 'chr3A,chr3B,chr3D'
+# ./chrProfilePlot_1homoeologuegroup_log2_histoneMod_IWGSCinput_x2_cMMb_step1Mb_defense_response_genes_altColours.R H3K27me1 H3K27me1_Rep1_ChIP H3K27me3 H3K27me3_ChIP_SRR6350666 MNase MNase_Rep1 input H3_input_SRR6350669 both 1Mb 1000000 15 navy firebrick1 200720 'Gypsy_LTR_RLG_subfamily_RLG_famc8.3' 'chr3A,chr3B,chr3D'
+# ./chrProfilePlot_1homoeologuegroup_log2_histoneMod_IWGSCinput_x2_cMMb_step1Mb_defense_response_genes_altColours.R H3K4me1 H3K4me1_Rep1_ChIP_SRR8126618 H3K27ac H3K27ac_Rep1_ChIP_SRR8126621 input H3_input_SRR6350669 input H3_input_SRR6350669 both 1Mb 1000000 15 magenta3 goldenrod1 200720 'Gypsy_LTR_RLG_subfamily_RLG_famc8.3' 'chr3A,chr3B,chr3D'
 
 #markChIPA <- "CENH3_ChIP_SRR1686799"
 #libNameChIPA <- "CENH3"
@@ -23,7 +23,7 @@
 #N <- 15
 #colourA <- "dodgerblue"
 #colourB <- "limegreen"
-#date <- "190620"
+#date <- "200720"
 #superfam <- "Gypsy_LTR_RLG_subfamily_RLG_famc8.3"
 #chrName <- unlist(strsplit("chr3A,chr3B,chr3D",
 #                           split = ","))
@@ -471,16 +471,16 @@ maxCPMB <- max(unlist(mclapply(which(chrs %in% chrName),
 }, mc.cores = length(filt_chrProfilesChIPB))))+0.025
 
 # Feature frequency chromosome profiles
-featureA <- read.table(paste0("/home/ajt200/analysis/wheat/chromosomeProfiles/cMMb/cMMb_iwgsc_refseqv1.0_mapping_data_minInterMarkerDist",
-                              "200bp_10Mb_step1Mb_alt.txt"),
-                       header = T)
-#featureA <- read.table("/home/ajt200/analysis/wheat/chromosomeProfiles/cMMb/iwgsc_refseqv1.0_recombination_rate.txt",
+#featureA <- read.table(paste0("/home/ajt200/analysis/wheat/chromosomeProfiles/cMMb/cMMb_iwgsc_refseqv1.0_mapping_data_minInterMarkerDist",
+#                              "200bp_10Mb_step1Mb.txt"),
 #                       header = T)
-#colnames(featureA) <- c("chr", "window", "intervalEnd", "nbOfSnps", "filt_feature")
+featureA <- read.table("/home/ajt200/analysis/wheat/chromosomeProfiles/cMMb/iwgsc_refseqv1.0_recombination_rate.txt",
+                       header = T)
+colnames(featureA) <- c("chr", "window", "intervalEnd", "nbOfSnps", "filt_feature")
 #featureA <- read.table(paste0("/home/ajt200/analysis/wheat/chromosomeProfiles/cMMb/cMMb_WGIN_CSxParagon_mapping_data_minInterMarkerDist",
 #                              "200bp_10Mb_step1Mb_alt.txt"),
 #                       header = T)
-colnames(featureA) <- c("chr", "window", "winEnd", "filt_feature")
+#colnames(featureA) <- c("chr", "window", "winEnd", "filt_feature")
 featureB <- read.table(paste0("/home/ajt200/analysis/wheat/chromosomeProfiles/genes/defense_response_gene_frequency_per_10Mb_step1Mb.txt"),
                        header = T)
 colnames(featureB) <- c("chr", "window", "filt_feature")
@@ -509,7 +509,8 @@ filt_chrProfilesFeatureA <- mclapply(seq_along(chrs), function(x) {
 #}, mc.cores = length(chrProfilesFeatureA))
 
 filt_chrProfilesFeatureB <- mclapply(seq_along(chrs), function(x) {
-  featureB[featureB$chr == chrs[x],]
+  featureB[featureB$chr == chrs[x] &
+           featureB[featureB$chr == chrs[x],]$window %in% featureA[featureA$chr == chrs[x],]$window,]
 }, mc.cores = length(chrs))
 
 #filt_chrProfilesFeatureB <- mclapply(seq_along(chrProfilesFeatureB), function(x) {
@@ -565,7 +566,7 @@ pdf(paste0(plotDir, "Wheat_", paste0(chrName, collapse = "_"),
            "_log2_", libNameChIPA, "_", libNameControlA,
            "_log2_", libNameChIPB, "_", libNameControlB, "_",
            align, "_featureFreq_chrPlot_winSize", winName, "_smooth", N,
-           "_CSxRenan_step1Mb_v", date, ".pdf"),
+           "_CSxRenan_step1Mb_IWGSCanalysis_v", date, ".pdf"),
     height = 4, width = 10*length(chrName))
 par(mfrow = c(1, length(chrName)))
 par(mar = c(5.0, 9.0, 2.1, 9.0))
@@ -608,7 +609,7 @@ pdf(paste0(plotDir, "Wheat",
            "_log2_", libNameChIPA, "_", libNameControlA,
            "_log2_", libNameChIPB, "_", libNameControlB, "_",
            align, "_featureFreq_chrPlot_winSize", winName, "_smooth", N,
-           "_CSxRenan_step1Mb_v", date, ".pdf"),
+           "_CSxRenan_step1Mb_IWGSCanalysis_v", date, ".pdf"),
     height = 4*7, width = 10*3)
 par(mfrow = c(7, 3))
 par(mar = c(5.0, 9.0, 2.1, 9.0))
