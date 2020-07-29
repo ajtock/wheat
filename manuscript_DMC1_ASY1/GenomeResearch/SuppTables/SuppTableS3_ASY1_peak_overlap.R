be0d1a74e60da97be671e4b06c503c3dd5263b30
ajtock <andytock@gmail.com>
2020-07-29 20:20:27 +0100

    SuppTableS1 alignment summary

---
+++ manuscript_DMC1_ASY1/GenomeResearch/SuppTables/SuppTableS3_ASY1_peak_overlap.R
@@ -0,0 +1,55 @@
+#!/applications/R/R-3.5.0/bin/Rscript
+
+# Compile ASY1 peak overlap permutation test results into one supplemental table
+
+subgenomes <- c("A", "B", "D")
+
+summaryTableDFList <- lapply(seq_along(subgenomes), function(x) {
+  R1R3 <- data.frame(Subgenome_compartments = paste0(subgenomes[x], "-genome R1 & R3"),
+                     rbind(read.table(paste0("/home/ajt200/analysis/wheat/ASY1_CS/snakemake_ChIPseq/mapped/both/peaks/",
+                                             "PeakRanger1.18/ranger/p0.001_q0.01/euchromatin/regioneR/noMinWidth_mergedOverlaps/",
+                                             "permTest_10000perms_ASY1_CS_Rep1_ChIP_peaks_vs_others_in_",
+                                             subgenomes[x],"genome_euchromatin_DataFrame.txt"), header = T, stringsAsFactors = F),
+                           read.table(paste0("/home/ajt200/analysis/wheat/ASY1_CS/snakemake_ChIPseq/mapped/both/peaks/",
+                                             "PeakRanger1.18/ranger/p0.001_q0.01/euchromatin/regioneR/noMinWidth_mergedOverlaps/",
+                                             "permTest_10000perms_ASY1_CS_Rep1_ChIP_peaks_vs_TEfams_in_",
+                                             subgenomes[x],"genome_euchromatin_DataFrame.txt"), header = T, stringsAsFactors = F)),
+                     stringsAsFactors = F)
+  R2C <- data.frame(Subgenome_compartments = paste0(subgenomes[x], "-genome R2a-R2b"),
+                    rbind(read.table(paste0("/home/ajt200/analysis/wheat/ASY1_CS/snakemake_ChIPseq/mapped/both/peaks/",
+                                            "PeakRanger1.18/ranger/p0.001_q0.01/heterochromatin/regioneR/noMinWidth_mergedOverlaps/",
+                                            "permTest_10000perms_ASY1_CS_Rep1_ChIP_peaks_vs_others_in_",
+                                            subgenomes[x],"genome_heterochromatin_DataFrame.txt"), header = T, stringsAsFactors = F),
+                          read.table(paste0("/home/ajt200/analysis/wheat/ASY1_CS/snakemake_ChIPseq/mapped/both/peaks/",
+                                            "PeakRanger1.18/ranger/p0.001_q0.01/heterochromatin/regioneR/noMinWidth_mergedOverlaps/",
+                                            "permTest_10000perms_ASY1_CS_Rep1_ChIP_peaks_vs_TEfams_in_",
+                                            subgenomes[x],"genome_heterochromatin_DataFrame.txt"), header = T, stringsAsFactors = F)),
+                    stringsAsFactors = F)
+  rbind(R1R3, R2C)
+})
+summaryTableDF <- do.call(rbind, summaryTableDFList)
+
+# Amend feature names for inclusion in table
+featureNames <- summaryTableDF$feature
+featureNames <- gsub(pattern = "promoters", replacement = "gene_promoters", x = featureNames)
+featureNames <- gsub(pattern = "terminators", replacement = "gene_terminators", x = featureNames)
+featureNames <- gsub(pattern = "TSSsPlus500bp", replacement = "gene_5'_ends", x = featureNames)
+featureNames <- gsub(pattern = "TTSsMinus500bp", replacement = "gene_3'_ends", x = featureNames)
+featureNames <- gsub(pattern = "NLRgene", replacement = "NLR-encoding_gene", x = featureNames)
+featureNames <- gsub(pattern = "meiogene", replacement = "Meiotic_gene", x = featureNames)
+featureNames <- gsub(pattern = "^NLRs$", replacement = "NLR-encoding_genes", x = featureNames)
+featureNames <- gsub(pattern = "^meio$", replacement = "Meiotic_genes", x = featureNames)
+featureNames <- gsub(pattern = "^gene", replacement = "Gene", x = featureNames)
+featureNames <- gsub(pattern = "H2AZ", replacement = "H2A.Z", x = featureNames)
+featureNames <- sub(pattern = "_\\w\\w\\w$", replacement = "", x = featureNames)
+featureNames <- gsub(pattern = "_", replacement = " ", x = featureNames)
+summaryTableDF$feature <- featureNames
+colnames(summaryTableDF) <- c("Subgenome compartments", "Feature", "Feature number",
+                              "Expected overlaps", "Observed overlaps",
+                              "P", "Local Z-score")
+write.table(summaryTableDF,
+            file = "Supplemental_TableS3_ASY1_peak_overlap.tsv",
+            col.names = T, row.names = F, sep = "\t", quote = F)
+write.csv(summaryTableDF,
+          file = "Supplemental_TableS3_ASY1_peak_overlap.csv",
+          row.names = F, quote = F)
---
+++ manuscript_DMC1_ASY1/GenomeResearch/SuppTables/SuppTableS4_cMMb_gene_quantiles_GO.R
@@ -0,0 +1,74 @@
+#!/applications/R/R-3.5.0/bin/Rscript
+
+# Compile cMMb gene quantile GO enrichment analysis results into one supplemental table
+
+library(GO.db)
+
+quantiles <- 1:4
+
+summaryTableDFList <- lapply(seq_along(quantiles), function(x) {
+  Wg <- data.frame(Subgenome = "All",
+                   cMMb_gene_quantile = paste0("Quantile ", x),
+                   read.table(paste0("/home/ajt200/analysis/wheat/ASY1_CS/snakemake_ChIPseq/mapped/geneProfiles_subgenomes/quantiles/",
+                                     "quantiles_by_cMMb/GO/featureIDs_quantile", x, "_of_4_by_cMMb_of_genes_in_",
+                                     "Agenome_Bgenome_Dgenome_genomewide_GO_BP_enrichment.tsv"),
+                                      header = T, sep = "\t", quote = "\"", check.names = F, stringsAsFactors = F),
+                   stringsAsFactors = F)
+  Ag <- data.frame(Subgenome = "A",
+                   cMMb_gene_quantile = paste0("Quantile ", x),
+                   read.table(paste0("/home/ajt200/analysis/wheat/ASY1_CS/snakemake_ChIPseq/mapped/geneProfiles_subgenomes/quantiles/",
+                                     "quantiles_by_cMMb/GO/featureIDs_quantile", x, "_of_4_by_cMMb_of_genes_in_",
+                                     "Agenome_genomewide_GO_BP_enrichment.tsv"),
+                                      header = T, sep = "\t", quote = "\"", check.names = F, stringsAsFactors = F),
+                   stringsAsFactors = F)
+  Bg <- data.frame(Subgenome = "B",
+                   cMMb_gene_quantile = paste0("Quantile ", x),
+                   read.table(paste0("/home/ajt200/analysis/wheat/ASY1_CS/snakemake_ChIPseq/mapped/geneProfiles_subgenomes/quantiles/",
+                                     "quantiles_by_cMMb/GO/featureIDs_quantile", x, "_of_4_by_cMMb_of_genes_in_",
+                                     "Bgenome_genomewide_GO_BP_enrichment.tsv"),
+                                      header = T, sep = "\t", quote = "\"", check.names = F, stringsAsFactors = F),
+                   stringsAsFactors = F)
+  Dg <- data.frame(Subgenome = "D",
+                   cMMb_gene_quantile = paste0("Quantile ", x),
+                   read.table(paste0("/home/ajt200/analysis/wheat/ASY1_CS/snakemake_ChIPseq/mapped/geneProfiles_subgenomes/quantiles/",
+                                     "quantiles_by_cMMb/GO/featureIDs_quantile", x, "_of_4_by_cMMb_of_genes_in_",
+                                     "Dgenome_genomewide_GO_BP_enrichment.tsv"),
+                                      header = T, sep = "\t", quote = "\"", check.names = F, stringsAsFactors = F),
+                   stringsAsFactors = F)
+  rbind(Wg, Ag, Bg, Dg)
+})
+summaryTableDF <- do.call(rbind, summaryTableDFList)
+# Remove additional test statistics from other methods ("classic", "elim", "weight")
+summaryTableDF <- summaryTableDF[,-c(8:11)]
+# Retain GO terms with topGO Fisher's exact test P-values <= 0.05
+summaryTableDF$topGOFisher <- sub(pattern = "< ", replacement = "", summaryTableDF$topGOFisher)
+summaryTableDF$topGOFisher <- as.numeric(summaryTableDF$topGOFisher)
+summaryTableDF <- summaryTableDF[summaryTableDF$topGOFisher <= 0.05,]
+
+# Replace truncated GO terms with full GO terms
+summaryTableDF <- data.frame(summaryTableDF,
+                             select(GO.db, keys = summaryTableDF$GO.ID,
+                                    columns = c("GOID", "TERM"),#"DEFINITION"),
+                                    keytype = "GOID"),
+                             stringsAsFactors = F) 
+# Ensure summaryTableDF$GO.ID matches the GO.db-obtained summaryTableDF$GOID,
+# and thus truncated GO terms correspond to the correct full GO terms
+stopifnot(all.equal(summaryTableDF$GO.ID, summaryTableDF$GOID))
+print("Number of topGO-derived truncated GO terms to be replaced with corresponding GO.db-obtained full GO terms =")
+print(length(summaryTableDF$Term[which(grepl("\\.\\.\\.", summaryTableDF$Term))]))
+print("Number of non-matching summaryTableDF$Term (original) and summaryTableDF$TERM (GO.db-obtained) GO terms =")
+print(all.equal(summaryTableDF$Term, summaryTableDF$TERM))
+summaryTableDF$Term <- summaryTableDF$TERM
+summaryTableDF <- summaryTableDF[,-c(9:10)]
+summaryTableDF <- summaryTableDF[,c(1:5, 7, 6, 8)]
+print(colnames(summaryTableDF))
+colnames(summaryTableDF) <- c("Subgenome", "cM/Mb gene quantile",
+                              "GO ID", "GO term",
+                              "Annotated genes in subgenome", "Expected genes in quantile", "Observed genes in quantile", "P")
+print(colnames(summaryTableDF))
+write.table(summaryTableDF,
+            file = "Supplemental_TableS4_cMMb_gene_quantiles_GO.tsv",
+            col.names = T, row.names = F, sep = "\t", quote = F)
+write.csv(summaryTableDF,
+          file = "Supplemental_TableS4_cMMb_gene_quantiles_GO.csv",
+          row.names = F, quote = F)
