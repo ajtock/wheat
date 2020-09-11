52c5d00ec4c8280edc7f1244fbee5c13d0eddaba
ajtock <andytock@gmail.com>
2020-09-11 11:50:11 +0100

    peak motifs

---
+++ DMC1/snakemake_ChIPseq/mapped/both/peaks/PeakRanger1.18/ranger/p0.001_q0.01/euchromatin/motifs_summits200bp/weeder2_bg_ranLoc_200bpseq/A/w2_summits200bpseq_top1000.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Usage:
+# ./w2_summits200bpseq_top1000.sh ASY1_CS_Rep1_ChIP_peaks_minuslog10Qsorted_in_Agenome_euchromatin_summits200bp
+# ./w2_summits200bpseq_top1000.sh ASY1_CS_Rep1_ChIP_peaks_minuslog10Qsorted_in_Agenome_euchromatin_summits200bp_shuffled
+
+prefix=$1
+toolDir="/home/ajt200/tools/Weeder2.0"
+
+weeder2 -f ${prefix}.fa \
+        -O TA -chipseq -top 1000
---
+++ DMC1/snakemake_ChIPseq/mapped/both/peaks/PeakRanger1.18/ranger/p0.001_q0.01/euchromatin/motifs_summits200bp/weeder2_bg_ranLoc_200bpseq/A/w2frequency_maker_ranLoc_200bpseq.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Usage:
+# ./w2frequency_maker_ranLoc_200bpseq.sh ASY1_CS_Rep1_ChIP_peaks_minuslog10Qsorted_in_Agenome_euchromatin_summits200bp
+
+prefix=$1
+toolDir="/home/ajt200/tools/Weeder2.0"
+
+[ -d FreqFiles ] || mkdir FreqFiles
+
+$toolDir/w2frequency_maker ${prefix}_randomLoci.fa TA ds
+
+mv *.freq FreqFiles
