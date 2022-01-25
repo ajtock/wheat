4c3affe6b14bd76325833832372fb8885343b78b
ajtock <andytock@gmail.com>
2022-01-25 18:22:16 +0000

    AxC table

--- tmp/AxC_markers-mapping-summary_mapped_markers.R
+++ tmp/AxC_markers-mapping-summary_mapped_markers.R
@@ -13,8 +13,73 @@ args <- commandArgs(trailingOnly = T)
 options(stringsAsFactors = F)
 library(data.table)
 
+
+# Genomic definitions
+chrs <- as.vector(read.table("/home/ajt200/analysis/wheat/sRNAseq_meiocyte_Martin_Moore/snakemake_sRNAseq/data/index/wheat_v1.0.fa.sizes")[,1])
+chrs <- chrs[-length(chrs)]
+chrLens <- as.vector(read.table("/home/ajt200/analysis/wheat/sRNAseq_meiocyte_Martin_Moore/snakemake_sRNAseq/data/index/wheat_v1.0.fa.sizes")[,2])
+chrLens <- chrLens[-length(chrLens)]
+centromereStart <- as.vector(read.table("/home/ajt200/analysis/wheat/wheat_IWGSC_WGA_v1.0_pseudomolecules/centromeres_outer_CENH3enriched_IWGSC_2018_Science_Table_S11_chr4ALeftmostInterval_chr5ARightTwoIntervals.txt")[,2])
+centromereEnd <- as.vector(read.table("/home/ajt200/analysis/wheat/wheat_IWGSC_WGA_v1.0_pseudomolecules/centromeres_outer_CENH3enriched_IWGSC_2018_Science_Table_S11_chr4ALeftmostInterval_chr5ARightTwoIntervals.txt")[,3])
+chrPartitions <- read.table("/home/ajt200/analysis/wheat/wheat_IWGSC_WGA_v1.0_pseudomolecules/chromosome_partitions_IWGSC_2018_Science_Table_S29.txt",
+                            header = TRUE)
+
+# Table of inter-marker cM values in WT and fancm
 tab <- fread("AxC_markers-mapping-summary_mapped_markers.tsv")
 colnames(tab) <- c("physical_marker", "chr", "pos", "wt_marker", "wt_cM", "fancm_marker", "fancm_cM")
 tab$pos <- as.integer(gsub(",", "", tab$pos))
 tab$chr <- sub("^", "chr", tab$chr)
-  
+
+ASY1 <- read.table("/home/ajt200/analysis/wheat/ASY1_CS/snakemake_ChIPseq/mapped/both/bg/ASY1_CS_Rep1_ChIP_MappedOn_wheat_v1.0_lowXM_both_sort_norm.bedgraph",
+                   header = F)
+
+# Rows where the difference between end and start coordinates is > winSize
+profileChIPA_bigWins <- profileChIPA[profileChIPA$V3-profileChIPA$V2 > winSize,]
+# Rows where the difference between end and start coordinates is == winSize
+profileChIPA <- profileChIPA[profileChIPA$V3-profileChIPA$V2 == winSize,]
+
+# Create a list of big windows, each split into windows of winSize,
+# or < winSize if at chromosome end
+profileChIPA_bigWinsList <- mclapply(seq_along(1:dim(profileChIPA_bigWins)[1]), function(x) {
+  bigWinsSplit <- seq(from = profileChIPA_bigWins[x,]$V2,
+                      to = profileChIPA_bigWins[x,]$V3,
+                      by = winSize)
+
+  if(bigWinsSplit[length(bigWinsSplit)] < profileChIPA_bigWins[x,]$V3) {
+    data.frame(V1 = as.character(profileChIPA_bigWins[x,]$V1),
+               V2 = as.integer(c(bigWinsSplit[-length(bigWinsSplit)],
+                                 bigWinsSplit[length(bigWinsSplit)])),
+               V3 = as.integer(c(bigWinsSplit[-length(bigWinsSplit)]+winSize,
+                                 profileChIPA_bigWins[x,]$V3)),
+               V4 = as.numeric(profileChIPA_bigWins[x,]$V4))
+  } else if (bigWinsSplit[length(bigWinsSplit)] == profileChIPA_bigWins[x,]$V3) {
+    data.frame(V1 = as.character(profileChIPA_bigWins[x,]$V1),
+               V2 = as.integer(bigWinsSplit[-length(bigWinsSplit)]),
+               V3 = as.integer(bigWinsSplit[-length(bigWinsSplit)]+winSize),
+               V4 = as.numeric(profileChIPA_bigWins[x,]$V4))
+  }
+}, mc.cores = detectCores())
+ 
+profileChIPA_bigWinsDT <- rbindlist(profileChIPA_bigWinsList)
+profileChIPA <- rbind.fill(profileChIPA, profileChIPA_bigWinsDT)
+profileChIPA <- profileChIPA[order(profileChIPA$V1, profileChIPA$V2),]
+
+chrLenValsAList <- mclapply(seq_along(chrs), function (x) {
+  chrProfileChIPA <- profileChIPA[profileChIPA$V1 == chrs[x],]
+  if(chrProfileChIPA[dim(chrProfileChIPA)[1],]$V3 < chrLens[x]) {
+    data.frame(V1 = chrs[x],
+               V2 = as.integer(chrProfileChIPA[dim(chrProfileChIPA)[1],]$V3),
+               V3 = as.integer(chrLens[x]),
+               V4 = as.numeric(chrProfileChIPA[dim(chrProfileChIPA)[1],]$V4))
+  }
+}, mc.cores = detectCores())
+profileChIPA_chrLenValsADT <- rbindlist(chrLenValsAList)
+profileChIPA <- rbind.fill(profileChIPA, profileChIPA_chrLenValsADT)
+profileChIPA <- profileChIPA[order(profileChIPA$V1, profileChIPA$V2),]
+
+profileChIPA <- data.frame(chr = as.character(profileChIPA$V1),
+                           window = as.integer(profileChIPA$V2+1),
+                           CPM = as.numeric(profileChIPA$V4),
+                           stringsAsFactors = F)
+
+
